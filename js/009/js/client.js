db546223f29605ba54ea2719d7fe36aa4de0e68b
refluster <refluster@gmail.com>
2013-11-21 07:27:53 +0900

    add samples 000-009

---
+++ js/009/js/client.js
@@ -0,0 +1,37 @@
+
+var conn = {}; // namespace
+
+(function($){
+    conn.socket = new io.connect("http://183.181.8.119:8081");
+    
+    conn.init = function() {
+        socket.on("connect", function(){
+            $("#transportName").text("connect via " + socket.socket.transport.name);
+        });
+        
+        socket.on("message", function(data){
+            var date = new Date;
+            var li = $("<li>");
+            li.html(li.text(data.message).html() 
+                    + '<small style="margin-left:30px;color:#a0a0a0"> by '
+                    + data.name + ' at ' + date.toString() + '</small>');
+            li.prependTo($("#messageArea ul"));
+        });
+        
+        $("#messageForm").submit(function(){
+            socket.emit("message", {message: $("#msg").val(), name: $("#name").val()});
+            $("#msg").val('');
+            return false;
+        });
+        
+        socket.on('user connected', function (data) {
+            jQuery('#member_count').html(data);
+        });
+        
+        socket.on('user disconnected', function (data) {
+            jQuery('#member_count').html(data);
+        });
+    };
+
+    
+})(jQuery);
