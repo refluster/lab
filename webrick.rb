f07f6dd4bb443b249e76b310ef51f15800979d53
refluster <refluster@gmail.com>
2014-05-31 10:05:53 +0900

    add webrick scripts to check behavior of http server

---
+++ webrick.rb
@@ -0,0 +1,7 @@
+require 'webrick'
+srv = WEBrick::HTTPServer.new({:DocumentRoot => './',
+                               :BindAddress => '192.168.13.100',
+                               :Port => 80})
+# srv.mount('/hoge.pl', WEBrick::HTTPServlet::CGIHandler, 'really_executed_script.rb')
+Signal.trap(:INT){ srv.shutdown }
+srv.start
