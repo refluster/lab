0f3ef99f543cfbb5b1c498b2b69239908eb80e16
refluster <refluster@gmail.com>
2014-10-13 18:00:53 +0900

    js/033: server side complete

--- js/033/js/server.js
+++ js/033/js/server.js
@@ -6,27 +6,35 @@ var unity;
 
 // Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
-    console.log("New connection")
-
-    conn.on("text", function (str) {
-        console.log("Received "+str)
-
-	msg = JSON.parse(str);
-	switch (msg.op) {
-	case "init":
-	    if (msg.type == "unity") {
-		unity = conn;
-	    } else if (msg.type == "sensor") {
-		sensor = conn;
-	    }
-	    break;
-	case "sensor":
-            unity.sendText(JSON.stringify(msg.data));
-	    break;
-	}
-    })
-
-    conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-    })
+	console.log("New connection")
+	
+	conn.on("text", function (str) {
+		console.log("Received "+str)
+		
+		msg = JSON.parse(str);
+		switch (msg.op) {
+		case "init":
+			if (msg.type == "unity") {
+				unity = conn;
+			} else if (msg.type == "sensor") {
+				sensor = conn;
+			}
+			break;
+		case "sensor":
+			if (unity) {
+				unity.sendText(JSON.stringify(msg.data));
+			}
+			break;
+		}
+	})
+	
+	conn.on("close", function (code, reason) {
+		if (conn == sensor) {
+			sensor = null;
+			console.log("Connection sensor closed")
+		} else if (conn == unity) {
+			unity = null;
+			console.log("Connection unity closed")
+		}
+	})
 }).listen(1338)
