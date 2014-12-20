3eace33c380777906abe9a5ea80683d58b68fbba
refluster <refluster@gmail.com>
2014-12-20 15:26:15 +0900

    test/js/034 draw rectangle

---
+++ js/034/js/draw.js
@@ -0,0 +1,56 @@
+var ratio = 0.93;
+
+// draw rect
+function drawRectCol(context, bx, by, width, height, iniDist) {
+	var cx, cy;
+	var length;
+	var distance;
+
+	cx = bx;
+	cy = by;
+	distance = iniDist;
+	length = iniDist*0.6;
+	
+	for (var i = 0; i < 40; i++) {
+//				context.fillRect(cx - length/2, cy - length/2, length, length);
+		context.beginPath();
+		context.moveTo(cx, cy - length/2);
+        context.lineTo(cx - length/2, cy);
+        context.lineTo(cx, cy + length/2);
+        context.lineTo(cx + length/2, cy);
+        context.closePath();
+        context.fill();
+		
+		distance *= ratio;
+		length *= ratio;
+		cy += distance;
+
+		if (length < 1 || cy > height) {
+			break;
+		}
+	}
+}
+
+function sample() {
+	// get canvas
+	var canvas = document.getElementById('sample1');
+	
+	if (canvas.getContext) {
+		var context = canvas.getContext('2d');
+
+		// set color
+		context.fillStyle = 'rgb(255,138,0)';
+		//context.strokeStyle = 'rgb(255,255,255)';
+
+		for (var i = true, x = 20; x < canvas.width; i = !i, x += 30) {
+			if (i == 0) {
+				drawRectCol(context, x, 20, canvas.width, canvas.height, 30);
+			} else {
+				drawRectCol(context, x, 20 + 30/2*ratio, canvas.width, canvas.height, 30);
+			}
+		}
+
+		console.log('done');
+	} 
+}
+
