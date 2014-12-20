ae1d9c72e730ff768789fffb8c5340d692cadc47
refluster <refluster@gmail.com>
2014-12-20 22:19:43 +0900

    test/js/034 add index item

--- js/034/js/draw.js
+++ js/034/js/draw.js
@@ -12,7 +12,6 @@ function drawRectCol(context, bx, by, width, height, iniDist) {
 	length = iniDist*0.6;
 	
 	for (var i = 0; i < 100; i++) {
-//				context.fillRect(cx - length/2, cy - length/2, length, length);
 		context.beginPath();
 		context.moveTo(cx, cy - length/2);
         context.lineTo(cx - length/2, cy);
@@ -31,16 +30,15 @@ function drawRectCol(context, bx, by, width, height, iniDist) {
 	}
 }
 
-function sample() {
+function draw() {
 	// get canvas
-	var canvas = document.getElementById('sample1');
+	var canvas = document.getElementById('canvas');
 	
 	if (canvas.getContext) {
 		var context = canvas.getContext('2d');
 
 		// set color
 		context.fillStyle = 'rgb(255,138,0)';
-		//context.strokeStyle = 'rgb(255,255,255)';
 
 		for (var i = true, x = 20; x < canvas.width; i = !i, x += 15) {
 			if (i == 0) {
