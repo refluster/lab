ec85dc128a0c783010b025c140ad3787d2b1916b
refluster <refluster@gmail.com>
2015-03-31 22:46:16 +0900

    test/js/038 layout

--- js/038/src/views/AppView.js
+++ js/038/src/views/AppView.js
@@ -1,59 +1,67 @@
 define(function(require, exports, module) {
 	var Transform = require('famous/core/Transform');
 	var Easing = require('famous/transitions/Easing');
-    var Lightbox = require('famous/views/Lightbox');
-    var View = require('famous/core/View');
+	var Lightbox = require('famous/views/Lightbox');
+	var View = require('famous/core/View');
 
-    var SlideView = require('views/SlideView');
+	var SlideView = require('views/SlideView');
 
-    function AppView() {
-        View.apply(this, arguments);
+	function AppView() {
+		View.apply(this, arguments);
+		
+		_createLightbox.call(this);
+		this.showSlide();
+	}
 
-        _createLightbox.call(this);
-        this.showSlide();
-    }
+	AppView.prototype = Object.create(View.prototype);
+	AppView.prototype.constructor = AppView;
 
-    AppView.prototype = Object.create(View.prototype);
-    AppView.prototype.constructor = AppView;
-
-    AppView.DEFAULT_OPTIONS = {
+	AppView.DEFAULT_OPTIONS = {
+		aspect: 2.5, // width / height
 		numSlide: 4,
-        lightboxOpts: {
-            //inOpacity: 0,
-            outOpacity: 0,
-            //inOrigin: [0, 0],
-            //outOrigin: [0, 0],
-            //showOrigin: [0, 0],
-            //inTransform: Transform.thenMove(Transform.rotateX(0.9), [0, 300, 0]),
-            //outTransform: Transform.thenMove(Transform.rotateZ(0.7), [0, window.innerHeight, 100]),
+		lightboxOpts: {
+			//inOpacity: 0,
+			outOpacity: 0,
+			//inOrigin: [0, 0],
+			//outOrigin: [0, 0],
+			//showOrigin: [0, 0],
+			//inTransform: Transform.thenMove(Transform.rotateX(0.9), [0, 300, 0]),
+			//outTransform: Transform.thenMove(Transform.rotateZ(0.7), [0, window.innerHeight, 100]),
 			//inTransform: Transform.thenMove(Transform.rotateX(0), [0, 0, -100]),
 			inTransform: Transform.thenMove(Transform.rotateX(0), [0, 0, 0]),
 			outTransform: Transform.thenMove(Transform.rotateZ(0), [0, 0, 100]),
-            inTransition: { duration: 650, curve: 'easeOut' },
-            outTransition: { duration: 300, curve: Easing.inCubic },
+			inTransition: { duration: 650, curve: 'easeOut' },
+			outTransition: { duration: 300, curve: Easing.inCubic },
 			overlap: false
-        }
-    };
+		}
+	};
 
-    function _createLightbox() {
-        this.lightbox = new Lightbox(this.options.lightboxOpts);
+	function _createLightbox() {
+		this.lightbox = new Lightbox(this.options.lightboxOpts);
 		//this.mainNode.add(this.lightbox);
 		this.add(this.lightbox);
-    }
+	}
 
 	AppView.prototype.showSlide = function() {
-        var slide = new SlideView({
-			size: [window.innerWidth * 0.8, window.innerHeight * 0.8]
+		var slideWidth;
+		if (window.innerHeight * this.options.aspect > window.innerWidth) {
+			slideWidth = window.innerWidth * 0.8;
+		} else {
+			slideWidth = window.innerHeight * this.options.aspect * 0.8;
+		}
+		
+		var slide = new SlideView({
+			size: [slideWidth, slideWidth / this.options.aspect]
 		});
 
 		slide.on('click', this.showSlide.bind(this));
 
-        this.ready = false;
-        this.lightbox.show(slide, function() {
-            this.ready = true;
-        }.bind(this));
-    };
+		this.ready = false;
+		this.lightbox.show(slide, function() {
+			this.ready = true;
+		}.bind(this));
+	};
 
-    module.exports = AppView;
+	module.exports = AppView;
 });
 
--- js/038/src/views/SlideView.js
+++ js/038/src/views/SlideView.js
@@ -5,41 +5,43 @@ define(function(require, exports, module) {
 	var StateModifier = require('famous/modifiers/StateModifier');
 	var Easing = require('famous/transitions/Easing');
 	var Transitionable = require('famous/transitions/Transitionable');
-    var View = require('famous/core/View');
+	var View = require('famous/core/View');
 	var GridLayout = require("famous/views/GridLayout");
 	var ImageSurface = require("famous/surfaces/ImageSurface");
-    var Transitionable   = require('famous/transitions/Transitionable');
-    var SpringTransition = require('famous/transitions/SpringTransition');
+	var Transitionable	 = require('famous/transitions/Transitionable');
+	var SpringTransition = require('famous/transitions/SpringTransition');
+	var FlexibleLayout = require('famous/views/FlexibleLayout');
 
 	var mCenter = new Modifier({
 		origin: [0.5,0.5],
 		align: [0.5,0.5],
 	});
 	
-    function SlideView() {
-        View.apply(this, arguments);
+	function SlideView() {
+		View.apply(this, arguments);
 
 		_layout.call(this);
 		_createTitleView.call(this);
 		_createDescriptionView.call(this);
 		_createDiagramView.call(this);
 		_createBackground.call(this);
-    }
+	}
 
-    SlideView.DEFAULT_OPTIONS = {
+	SlideView.DEFAULT_OPTIONS = {
 		textColor: '#fff',
 		size: [800, 600],
-		duration: 600,
-		tick: 400,
-    };
+		duration: 300,
+		tick: 300,
+		border: '0px solid white'
+	};
 
 	SlideView.DEFAULT_OPTIONS.transition = {
 		duration: SlideView.DEFAULT_OPTIONS.duration,
 		curve: 'easeOut'
 	};
 	
-    SlideView.prototype = Object.create(View.prototype);
-    SlideView.prototype.constructor = SlideView;
+	SlideView.prototype = Object.create(View.prototype);
+	SlideView.prototype.constructor = SlideView;
 
 	function _createBackground() {
 		var sBackground = new Surface({
@@ -53,8 +55,8 @@ define(function(require, exports, module) {
 		this.add(sBackground);
 
 		sBackground.on('click', function() {
-            this._eventOutput.emit('click');
-        }.bind(this));
+			this._eventOutput.emit('click');
+		}.bind(this));
 	}
 	
 	function _layout() {
@@ -63,144 +65,149 @@ define(function(require, exports, module) {
 		this.vDescription = new View();
 		this.vDiagram = new View();
 
-		var FlexibleLayout = require('famous/views/FlexibleLayout');
 		var layout = new FlexibleLayout({
-			ratios: [2, 3],
+			ratios: [1, 3],
 			direction: 1
 		});
 		layout.sequenceFrom([this.vTitle, this.vDescription]);
 		
-        var centerModifier = new Modifier({
+		var centerModifier = new Modifier({
 			origin: [0.5, 0.5],
 			align: [0.5, 0.5],
 			size: this.options.size
-        });
-        var grid = new GridLayout({
-            dimensions: [2, 1]
-        });
-        grid.sequenceFrom([layout, this.vDiagram]);
-		
-		this.add(centerModifier).add(grid);
+		});
+		var layout2 = new FlexibleLayout({
+			ratios: [3, 4]
+		});
+		layout2.sequenceFrom([layout, this.vDiagram]);
+		this.add(centerModifier).add(layout2);
 	}
 	
 	function _createTitleView() {
 		var sTitle = new Surface({
 			content: 'Flexible Animation',
 			properties: {
-				fontSize: '40px',
+				fontSize: (this.options.size[0]*0.04) + 'px',
 				color: this.options.textColor,
-				border: '1px solid white',
-				lineHeight: '100px',
-				//textAlign: 'center',
+				border: this.options.border,
 				pointerEvents: 'none'
 			}
 		});
-
+		
 		var smInsert = new StateModifier({
 			transform: Transform.translate(0, 0, -100)
 		});
 
 		smInsert.setTransform(
 			Transform.translate(0, 0, 0),
-			{
-				duration: this.options.duration,
-				curve: 'easeInOut'
-			}
+			SlideView.DEFAULT_OPTIONS.transition
 		);
 
 		this.vTitle.add(smInsert).add(sTitle);
 	}
 
 	function _createDescriptionView() {
+		var code = "<pre>" +
+			"aaa\n" +
+			"  aaa\n" +
+			"	 aaa\n" +
+			"</pre>";
+
 		var sDescription = new Surface({
-			content: 'Contents can be animated smoothly with famo.us framework',
+			content: 'Contents can be animated smoothly with famo.us framework' + code,
 			properties: {
+				fontSize: (this.options.size[0]*0.02) + 'px',
 				color: this.options.textColor,
-				border: '1px solid white',
+				border: this.options.border,
 				pointerEvents: 'none'
 			}
 		});
 		
 		var smInsert = new StateModifier({
+			opacity: 0,
 			transform: Transform.translate(0, 0, -100)
 		});
 
-		smInsert.setTransform(
-			Transform.translate(0, 0, 0),
-			{
-				duration: this.options.duration,
-				curve: 'easeInOut'
-			}
-		);
+		setTimeout(function() {
+			smInsert.setOpacity(1, SlideView.DEFAULT_OPTIONS.transition)
+			smInsert.setTransform(Transform.translate(0, 0, 0), SlideView.DEFAULT_OPTIONS.transition)
+		}, this.options.tick*2);
+		
 
 		this.vDescription.add(smInsert).add(sDescription);
 	}
 
 	function _createDiagramView() {
-		var initialTime = Date.now();
-		
 		////////////////////////////////////////////////////////////
 		// scheme-mono rotate
-		var logo = new ImageSurface({
-			size: [300, 100],
+		var isLogo = new ImageSurface({
+			size: [this.options.size[0]*0.42, true],
 			content: 'img/scheme-mono.png',
 			classes: ['double-sided']
 		});
 
 		var mCenterSpinY = new Modifier({
 			origin: [0.5, 0.5],
-			align: [0.5, 0.3],
-			transform: function () {
-				return Transform.rotateY(.002 * (Date.now() - initialTime));
-			}
+			align: [0.5, 0.25],
 		});
 
-		var smAppear = new StateModifier({
+		var smInsert = new StateModifier({
 			opacity: 0,
 			transform: Transform.translate(100, 0, 0)
 		});
 
 		setTimeout(function() {
 			initialTime = Date.now();
-			smAppear.setTransform(
-				Transform.translate(0, 0, 0),
-				SlideView.DEFAULT_OPTIONS.transition);
-			smAppear.setOpacity(1, SlideView.DEFAULT_OPTIONS.transition);
+			smInsert.setOpacity(1, SlideView.DEFAULT_OPTIONS.transition);
+			smInsert.setTransform(Transform.translate(0, 0, 0),
+								  SlideView.DEFAULT_OPTIONS.transition,
+								  function() {
+									  var initialTime = Date.now();
+									  mCenterSpinY.setTransform(
+										  function() {
+											  return Transform.rotateY(.002 * (Date.now() - initialTime));
+										  });
+								  }
+								 );
 		}, this.options.tick*4);
-
-		this.vDiagram.add(smAppear).add(mCenterSpinY).add(logo);
-
+		
+		this.vDiagram.add(smInsert).add(mCenterSpinY).add(isLogo);
+		
 		////////////////////////////////////////////////////////////
-		// restroom
-		var logo2 = new ImageSurface({
+		// rest room
+		var isLogo2 = new ImageSurface({
 			size: [this.options.size[0]*0.1, true],
 			content: 'http://illustcut.com/box/mark/iroiro1/mark01_20.png'
 		});
 		
-		var smDamping = new StateModifier({
-			origin: [0.5, 0],
-			align: [0.5, 0.6],
-			transform: Transform.translate(100, 0, 0)
+		var smDamping = new StateModifier();
+
+		var smInsert2 = new StateModifier({
+			origin: [0.5, 0.5],
+			align: [0.5, 0.75],
+			opacity: 0,
+			transform: Transform.translate(150, 0, 0)
 		});
 
 		Transitionable.registerMethod('spring', SpringTransition);
 		
-		smDamping.setTransform(
-			Transform.rotateX(-0.5),
-			{ duration: 200, curve: 'easeOut' }
-		);
-		
-		smDamping.setTransform(
-			Transform.identity,
-			{ method: 'spring', period: 1100, dampingRatio: 0 }
-		);
-		
 		setTimeout(function() {
-			smDamping.setOpacity(1, SlideView.DEFAULT_OPTIONS.transition);
-			smDamping.setTransform(Transform.translate(0, 0, 0), SlideView.DEFAULT_OPTIONS.transition);
-		}, this.options.tick*6);
+			smInsert2.setOpacity(1, SlideView.DEFAULT_OPTIONS.transition);
+			smInsert2.setTransform(Transform.translate(0, 0, 0), SlideView.DEFAULT_OPTIONS.transition,
+								   function() {
+									   smDamping.setTransform(
+										   Transform.rotateX(-0.5),
+										   { duration: 200, curve: 'easeOut' }
+									   );
+									   smDamping.setTransform(
+										   Transform.identity,
+										   { method: 'spring', period: 1100, dampingRatio: 0 }
+									   );
+								   }
+								  );
+		}, this.options.tick*3);
 		
-		this.vDiagram.add(smDamping).add(logo2);
+		this.vDiagram.add(smDamping).add(smInsert2).add(isLogo2);
 	}
 	
 	module.exports = SlideView;
