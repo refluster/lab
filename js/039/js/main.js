7f10474a1a7f3f1cf178c93f158fd958ae44a629
refluster <refluster@gmail.com>
2015-05-08 16:32:59 +0900

    IssueID #32 : refactor js class design

--- js/039/js/main.js
+++ js/039/js/main.js
@@ -1,10 +1,12 @@
+
+
 $(function(){
 	function disableScrolling(e) { e.preventDefault();}
 	document.addEventListener('touchstart', disableScrolling, false);
 	document.addEventListener('touchmove', disableScrolling, false);
 	$('img').on('dragstart', disableScrolling);
 
-	function movable(id, update) {
+	var Movable = function(id, update) {
 		this.state = {
 			offset_x: 0,
 			last_margin_x: 0,
@@ -13,37 +15,37 @@ $(function(){
 		this.bodyObj = $('body');
 		this.update = update;
 
-		this.obj.mousedown(inputStart.bind(this));
-		this.obj.bind('touchstart', inputStart.bind(this));
-
-		function inputStart(e) {
-			var pageX = (e.pageX? e.pageX: e.originalEvent.touches[0].pageX);
-
-			this.state.offset_x = pageX - this.state.last_margin_x;
-			this.bodyObj.mousemove(inputMove.bind(this));
-			this.bodyObj.mouseup(inputEnd.bind(this));
-			this.bodyObj.bind('touchmove', inputMove.bind(this));
-			this.bodyObj.bind('touchend', inputEnd.bind(this));
-		}
-
-		function inputMove(e) {
-			var pageX = (e.pageX? e.pageX: e.originalEvent.touches[0].pageX);
-
-			this.state.last_margin_x = pageX - this.state.offset_x;
-			this.obj.css({'margin-left': this.state.last_margin_x + 'px'});
-			this.update({left: this.state.last_margin_x / this.bodyObj.width(),
-						 right: (this.state.last_margin_x + this.obj.width()) / this.bodyObj.width()});
-		}
-
-		function inputEnd(e) {
-			this.bodyObj.unbind('mousemove');
-			this.bodyObj.unbind('mouseup');
-			this.bodyObj.unbind('touchmove');
-			this.bodyObj.unbind('touchend');
-		}
+		this.obj.mousedown(this.inputStart.bind(this));
+		this.obj.bind('touchstart', this.inputStart.bind(this));
 	}
 
-	function ledCtrl() {
+	Movable.prototype.inputStart = function(e) {
+		var pageX = (e.pageX? e.pageX: e.originalEvent.touches[0].pageX);
+		
+		this.state.offset_x = pageX - this.state.last_margin_x;
+		this.bodyObj.mousemove(this.inputMove.bind(this));
+		this.bodyObj.mouseup(this.inputEnd.bind(this));
+		this.bodyObj.bind('touchmove', this.inputMove.bind(this));
+		this.bodyObj.bind('touchend', this.inputEnd.bind(this));
+	}
+	
+	Movable.prototype.inputMove = function(e) {
+		var pageX = (e.pageX? e.pageX: e.originalEvent.touches[0].pageX);
+		
+		this.state.last_margin_x = pageX - this.state.offset_x;
+		this.obj.css({'margin-left': this.state.last_margin_x + 'px'});
+		this.update({left: this.state.last_margin_x / this.bodyObj.width(),
+					 right: (this.state.last_margin_x + this.obj.width()) / this.bodyObj.width()});
+	}
+	
+	Movable.prototype.inputEnd = function(e) {
+		this.bodyObj.unbind('mousemove');
+		this.bodyObj.unbind('mouseup');
+		this.bodyObj.unbind('touchmove');
+		this.bodyObj.unbind('touchend');
+	}
+	
+	function LedCtrl() {
 		this.refColor = [];
 		this.refColor.push({ratio: 0,
 							rgb: [{r:   0, g:   0, b:   0},
@@ -69,43 +71,50 @@ $(function(){
 					{r: 0, g: 0, b: 0},
 					{r: 0, g: 0, b: 0},
 					{r: 0, g: 0, b: 0}];
+		this.ledSun = [{r: 0, g: 0, b: 0},
+					   {r: 0, g: 0, b: 0},
+					   {r: 0, g: 0, b: 0},
+					   {r: 0, g: 0, b: 0},
+					   {r: 0, g: 0, b: 0}];
 
-		this.updateSun = function(p) {
-			var r = (p.left + p.right)/2;
+		this.sunX = 0;
+	}
 
-			for (var i = 0; i < this.refColor.length - 1; i++) {
-				if (r < this.refColor[i + 1].ratio) {
-					var c = this.refColor[i + 1].ratio - this.refColor[i].ratio;
-					var a = r - this.refColor[i].ratio;
-					var b = this.refColor[i + 1].ratio - r;
-					var ra = a/c;
-					var rb = b/c;
-			
-					for (var j = 0; j < this.ledSun.length; j++) {
-						this.ledSun[j].r = Math.floor(this.refColor[i].rgb[j].r*rb +
-													  this.refColor[i + 1].rgb[j].r*ra);
-						this.ledSun[j].g = Math.floor(this.refColor[i].rgb[j].g*rb +
-													  this.refColor[i + 1].rgb[j].g*ra);
-						this.ledSun[j].b = Math.floor(this.refColor[i].rgb[j].b*rb +
-													  this.refColor[i + 1].rgb[j].b*ra);
-					}
+	LedCtrl.prototype.updateSun = function(p) {
+		this.sunX = (p.left + p.right)/2;
+		this.updateLed.bind(this);
+	}
 
-					console.log(this.ledSun[0]);
-					break;
+	LedCtrl.prototype.updateCloud = function(p) {
+		//this.sunX = (p.left + p.right)/2;
+		this.updateLed.bind(this);
+	}
+
+	LedCtrl.prototype.updateLed = function() {
+		for (var i = 0; i < this.refColor.length - 1; i++) {
+			if (this.sunX < this.refColor[i + 1].ratio) {
+				var c = this.refColor[i + 1].ratio - this.refColor[i].ratio;
+				var a = this.sunX - this.refColor[i].ratio;
+				var b = this.refColor[i + 1].ratio - this.sunX;
+				var ra = a/c;
+				var rb = b/c;
+				
+				for (var j = 0; j < this.ledSun.length; j++) {
+					this.ledSun[j].r = Math.floor(this.refColor[i].rgb[j].r*rb +
+												  this.refColor[i + 1].rgb[j].r*ra);
+					this.ledSun[j].g = Math.floor(this.refColor[i].rgb[j].g*rb +
+												  this.refColor[i + 1].rgb[j].g*ra);
+					this.ledSun[j].b = Math.floor(this.refColor[i].rgb[j].b*rb +
+												  this.refColor[i + 1].rgb[j].b*ra);
 				}
+				
+				console.log(this.ledSun[0]);
+				break;
 			}
-		}
-		
-		this.updateCloud = function(p) {
-			console.log(p);
 		}
 	}
 
-	var led = new ledCtrl();
-
-	led.updateSun(0);
-	var sun = new movable('#sun', led.updateSun.bind(led));
-	var cloud = new movable('#cloud', led.updateCloud.bind(led));
-
-	$('#color_disp_0').text('hoge');
+	var led = new LedCtrl();
+	var sun = new Movable('#sun', led.updateSun.bind(led));
+	var cloud = new Movable('#cloud', led.updateCloud.bind(led));
 });
