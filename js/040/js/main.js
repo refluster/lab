6b553b8b5e4a3b5487e7ab5f903942a41f153b14
refluster <refluster@gmail.com>
2015-06-04 08:14:59 +0900

    test/js/040 refactor css

--- js/040/js/main.js
+++ js/040/js/main.js
@@ -8,14 +8,13 @@ window.onload = function() {
 	});
 
 	$("#circle").click(function(e) {
-		$("#circle").addClass("circle-big");
+		$("#circle").addClass("scale");
 	});
 
 	// Get context with jQuery - using jQuery's .get() method.
 	var ctx = $("#myChart").get(0).getContext("2d");
 	// This will get the first returned node in the jQuery collection.
 	var myNewChart = new Chart(ctx);
-//	new Chart(ctx).PolarArea(data, options);
 
 	Chart.defaults.global = {
 		// Boolean - Whether to animate the chart
