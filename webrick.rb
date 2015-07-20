d931f03b1defa8b5f7614526beff61aabd49cc95
refluster <refluster@gmail.com>
2015-07-20 15:22:44 +0900

    insert tracking code google analytics

--- webrick.rb
+++ webrick.rb
@@ -1,6 +1,6 @@
 require 'webrick'
 srv = WEBrick::HTTPServer.new({:DocumentRoot => './',
-                               :BindAddress => '192.168.13.100',
+                               :BindAddress => '192.168.13.67',
                                :Port => 80})
 # srv.mount('/hoge.pl', WEBrick::HTTPServlet::CGIHandler, 'really_executed_script.rb')
 Signal.trap(:INT){ srv.shutdown }
