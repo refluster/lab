83837b48999b6e9fd267dcae594e78b96a07cc3a
refluster <refluster@gmail.com>
2015-08-08 11:02:08 +0900

    lab/js/001 refactor as object orient code

--- js/001/js/cv.js
+++ js/001/js/cv.js
@@ -1,97 +1,10 @@
 /* HTML5 Canvas drag&drop
  * canvas is updated when an object is dragged by 1px
  */
-var panelApl = {}; // namespace
-
-/* global var */
-// drag state
-panelApl.drag = {
-	now: false, // true if dragging
-	item: null  // index of itemAr of dragging item
-};
-panelApl.gamestart = false;  // true if playing
-
-/* button process
- * return: none
- */
-panelApl.start = function() {
-	var $cvdiv = $('#cvdiv1');
-	// set events to the canvas
-	$cvdiv.mousedown(panelApl.cvmsDown);
-	$cvdiv.mouseup(panelApl.cvmsUp);
-	$cvdiv.mouseleave(panelApl.cvmsUp);
-	$cvdiv.mousemove(panelApl.cvmsMove);
-	// init canvas
-	panelApl.DRAG.init();
-	panelApl.DRAG.draw();
+var panelApl = function() {
+	this.dragging = false;
+	this.dragItem = null;
 	
-	panelApl.gamestart = true;
-};
-
-/* mousedown process
- * {event} evt: event obj
- * return: none
- */
-panelApl.cvmsDown = function(evt) {
-	if (!panelApl.drag.now) {
-		// convert coordinate from point to canvas
-		var cx = evt.pageX - panelApl.DRAG.cvpos.x;
-		var cy = evt.pageY - panelApl.DRAG.cvpos.y;
-		// check if any object is at the point
-		var itemIdx = panelApl.DRAG.checkItem(cx, cy);
-		if (itemIdx != null) {
-			panelApl.drag.now = true;
-			panelApl.drag.item = itemIdx;
-		}
-	}
-	return false;
-};
-/* mouseup/mouseleave process
- * {event} evt: event obj
- * return: none
- */
-panelApl.cvmsUp = function(evt) {
-	if (panelApl.drag.now) {
-		// convert coordinate from point to canvas
-		var cx = evt.pageX - panelApl.DRAG.cvpos.x;
-		var cy = evt.pageY - panelApl.DRAG.cvpos.y;
-		if (cx < 0) cx = 0;
-		if (cx > panelApl.DRAG.area.w) cx = panelApl.DRAG.area.w;
-		if (cy < 0) cy = 0;
-		if (cy > panelApl.DRAG.area.h) cy = panelApl.DRAG.area.h;
-		// set item position to the center of the grid
-		//panelApl.DRAG.setCenter(panelApl.drag.item, cx, cy);
-		// update canvas
-		panelApl.DRAG.draw();
-
-		panelApl.drag.now = false;
-		panelApl.drag.item = null;
-	}
-};
-/* mousemove process
- * {event} evt: evnet obj
- * return: none
- */
-panelApl.cvmsMove = function(evt) {
-	if (panelApl.drag.now) {
-		// convert coordinate from point to canvas
-		var cx = evt.pageX - panelApl.DRAG.cvpos.x;
-		var cy = evt.pageY - panelApl.DRAG.cvpos.y;
-		// check if the canvas should be updated
-		var updSep = 1; // #. of pixels that canvas is updated if an object is moved by
-		if (Math.abs(cx - panelApl.DRAG.itemAr[panelApl.drag.item].x) >= updSep ||
-			Math.abs(cy - panelApl.DRAG.itemAr[panelApl.drag.item].y) >= updSep) {
-			// update the position of the item
-			panelApl.DRAG.itemAr[panelApl.drag.item].x = cx;
-			panelApl.DRAG.itemAr[panelApl.drag.item].y = cy;
-			// update the canvas
-			panelApl.DRAG.draw();
-		}
-	}
-	return false;
-};
-
-$(function() {
 	// get canvas's DOM element and context
 	var canvas = $('#cv1')[0];
 	if ( ! canvas || ! canvas.getContext ) { return false; }
@@ -101,9 +14,95 @@ $(function() {
 	ctx.globalCompositeOperation = "source-over";
 	
 	// display
-	panelApl.DRAG = new canvasManager.canv(ctx, canvas.width, canvas.height, panelApl);
-	panelApl.DRAG.init();
-	panelApl.DRAG.draw();
+	this.DRAG = new canvasManager.canv(ctx, canvas.width, canvas.height, this);
+	this.DRAG.init();
+	this.DRAG.draw();
 	
-	panelApl.start();
+	this.start();
+
+};
+
+
+/* button process
+ * return: none
+ */
+panelApl.prototype.start = function() {
+	var $cvdiv = $('#cvdiv1');
+	// set events to the canvas
+	$cvdiv.mousedown(this.cvmsDown.bind(this));
+	$cvdiv.mouseup(this.cvmsUp.bind(this));
+	$cvdiv.mouseleave(this.cvmsUp.bind(this));
+	$cvdiv.mousemove(this.cvmsMove.bind(this));
+	// init canvas
+	this.DRAG.init();
+	this.DRAG.draw();
+	
+	this.gamestart = true;
+};
+
+/* mousedown process
+ * {event} evt: event obj
+ * return: none
+ */
+panelApl.prototype.cvmsDown = function(evt) {
+	if (!this.dragging) {
+		// convert coordinate from point to canvas
+		var cx = evt.pageX - this.DRAG.cvpos.x;
+		var cy = evt.pageY - this.DRAG.cvpos.y;
+		// check if any object is at the point
+		var itemIdx = this.DRAG.checkItem(cx, cy);
+		if (itemIdx != null) {
+			this.dragging = true;
+			this.dragItem = itemIdx;
+		}
+	}
+	return false;
+};
+/* mouseup/mouseleave process
+ * {event} evt: event obj
+ * return: none
+ */
+panelApl.prototype.cvmsUp = function(evt) {
+	if (this.dragging) {
+		// convert coordinate from point to canvas
+		var cx = evt.pageX - this.DRAG.cvpos.x;
+		var cy = evt.pageY - this.DRAG.cvpos.y;
+		if (cx < 0) cx = 0;
+		if (cx > this.DRAG.area.w) cx = this.DRAG.area.w;
+		if (cy < 0) cy = 0;
+		if (cy > this.DRAG.area.h) cy = this.DRAG.area.h;
+		// set item position to the center of the grid
+		//this.DRAG.setCenter(this.drag.item, cx, cy);
+		// update canvas
+		this.DRAG.draw();
+
+		this.dragging = false;
+		this.dragItem = null;
+	}
+};
+/* mousemove process
+ * {event} evt: evnet obj
+ * return: none
+ */
+panelApl.prototype.cvmsMove = function(evt) {
+	if (this.dragging) {
+		// convert coordinate from point to canvas
+		var cx = evt.pageX - this.DRAG.cvpos.x;
+		var cy = evt.pageY - this.DRAG.cvpos.y;
+		// check if the canvas should be updated
+		var updSep = 1; // #. of pixels that canvas is updated if an object is moved by
+		if (Math.abs(cx - this.DRAG.itemAr[this.dragItem].x) >= updSep ||
+			Math.abs(cy - this.DRAG.itemAr[this.dragItem].y) >= updSep) {
+			// update the position of the item
+			this.DRAG.itemAr[this.dragItem].x = cx;
+			this.DRAG.itemAr[this.dragItem].y = cy;
+			// update the canvas
+			this.DRAG.draw();
+		}
+	}
+	return false;
+};
+
+$(function() {
+	var panel = new panelApl();
 });
