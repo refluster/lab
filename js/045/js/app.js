09b4b5e08653e1d683a9c7fe9ce139e92eb33be5
refluster <refluster@gmail.com>
2015-08-12 16:40:06 +0900

    lab/js/045 set angularjs controller to each view

--- js/045/js/app.js
+++ js/045/js/app.js
@@ -3,15 +3,22 @@ app = angular.module('App', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'tmpl-list-wide'
+			templateUrl: 'tmpl-list-wide',
+			controller: 'ListWideController'
 		})
 		.when('/list-narrow', {
-			templateUrl: 'tmpl-list-narrow'
+			templateUrl: 'tmpl-list-narrow',
+			controller: 'ListNarrowController'
 		})
 		.when('/show/:id', {
-			templateUrl: 'tmpl-show'
+			templateUrl: 'tmpl-show',
+			controller: 'ShowController'
 		})
 		.otherwise({
 			redirectTo: '/'
 		});
 }]);
+
+app.controller('ListWideController', [function ListWideController() {}])
+app.controller('ListNarrowController', [function ListNarrowController() {}])
+app.controller('ShowController', [function ShowController() {}]);
