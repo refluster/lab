7c87a542ecc979c95ae02423e14ad92fdfc0ee8e
refluster <refluster@gmail.com>
2015-08-12 17:21:44 +0900

    lab/js/045 display wide view with picture array sourced by list service

--- js/045/js/app.js
+++ js/045/js/app.js
@@ -19,6 +19,47 @@ app.config(['$routeProvider', function ($routeProvider) {
 		});
 }]);
 
-app.controller('ListWideController', [function ListWideController() {}])
-app.controller('ListNarrowController', [function ListNarrowController() {}])
-app.controller('ShowController', [function ShowController() {}]);
+app.service('list', ['$rootScope', '$filter', function($scope, $filter) {
+	this.list = [];
+
+    $scope.$watch(function () {
+        return this.list;
+    }.bind(this), function (value) {
+        $scope.$broadcast('change:list', value);
+    }.bind(this), true);
+	
+	this.load = function() {
+		this.list = [{
+			date: '2015/08/12',
+			file: [
+				['build000.jpg','build000-80.jpg'],
+				['build001.jpg','build001-80.jpg'],
+				['build002.jpg','build002-80.jpg'],
+				['build003.jpg','build003-80.jpg'],
+				['build004.jpg','build004-80.jpg'],
+				['build005.jpg','build005-80.jpg'],
+				['build006.jpg','build006-80.jpg']
+			]}];
+	};
+
+	this.get = function() {
+		return this.list;
+	};
+}]);
+
+app.controller('ListWideController', ['$scope', 'list', function($scope, list) {
+/*
+    $scope.$on('change:list', function (e, list) {
+		$scope.pictureList = list;
+    });
+*/
+
+	list.load();
+	$scope.pictureList = list.get();
+}]);
+
+app.controller('ListNarrowController', [function() {
+}]);
+
+app.controller('ShowController', [function() {
+}]);
