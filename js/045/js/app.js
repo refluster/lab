214160bae313e4c2ffa48f6c46b14ec6104ebfa0
refluster <refluster@gmail.com>
2015-08-12 17:49:47 +0900

    lab/js/045 display for each picture for each date

--- js/045/js/app.js
+++ js/045/js/app.js
@@ -38,7 +38,37 @@ app.service('list', ['$rootScope', '$filter', function($scope, $filter) {
 				['build003.jpg','build003-80.jpg'],
 				['build004.jpg','build004-80.jpg'],
 				['build005.jpg','build005-80.jpg'],
-				['build006.jpg','build006-80.jpg']
+				['build006.jpg','build006-80.jpg'],
+			]}, {
+			date: '2015/08/13',
+			file: [
+				['drink000.jpg','drink000-80.jpg'],
+				['drink001.jpg','drink001-80.jpg'],
+				['drink002.jpg','drink002-80.jpg'],
+				['drink003.jpg','drink003-80.jpg'],
+				['drink004.jpg','drink004-80.jpg'],
+				['drink005.jpg','drink005-80.jpg'],
+				['drink006.jpg','drink006-80.jpg'],
+				['drink007.jpg','drink007-80.jpg'],
+				['drink008.jpg','drink008-80.jpg'],
+				['drink009.jpg','drink009-80.jpg'],
+				['drink010.jpg','drink010-80.jpg'],
+				['park000.jpg','park000-80.jpg'],
+				['park001.jpg','park001-80.jpg'],
+				['park002.jpg','park002-80.jpg'],
+				['park003.jpg','park003-80.jpg'],
+				['park004.jpg','park004-80.jpg'],
+				['park005.jpg','park005-80.jpg'],
+				['park006.jpg','park006-80.jpg'],
+				['park007.jpg','park007-80.jpg'],
+				['park008.jpg','park008-80.jpg'],
+				['park009.jpg','park009-80.jpg'],
+				['park010.jpg','park010-80.jpg'],
+				['park011.jpg','park011-80.jpg'],
+				['park012.jpg','park012-80.jpg'],
+				['park013.jpg','park013-80.jpg'],
+				['park014.jpg','park014-80.jpg'],
+				['park015.jpg','park015-80.jpg'],
 			]}];
 	};
 
