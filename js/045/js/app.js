7d27c8d5ac20ae6704036325aa2cf9ea181e1d39
refluster <refluster@gmail.com>
2015-08-12 16:29:02 +0900

    lab/js/045 implement basic views

--- js/045/js/app.js
+++ js/045/js/app.js
@@ -1,3 +1,17 @@
 app = angular.module('App', ['ngRoute']);
 
-
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'tmpl-list-wide'
+		})
+		.when('/list-narrow', {
+			templateUrl: 'tmpl-list-narrow'
+		})
+		.when('/show/:id', {
+			templateUrl: 'tmpl-show'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
