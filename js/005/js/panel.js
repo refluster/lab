06bf1abaf03c6af870454b1c1f91c941326ac928
refluster <refluster@gmail.com>
2015-08-16 20:38:29 +0900

    lab/js/005 remove unused global vars and mouse/touch event callback

--- js/005/js/panel.js
+++ js/005/js/panel.js
@@ -4,13 +4,6 @@
 var panelApl = {}; // namespace
 
 (function($) {
-
-    /* global var */
-    // drag state
-    panelApl.drag = {
-		now: false, // true if dragging
-    };
-
     // timer
     panelApl.timer = $.timer();
 
@@ -19,69 +12,11 @@ var panelApl = {}; // namespace
      */
     panelApl.start = function() {
 		var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
-		var $btn = $('#stbtn1'); // start button
 
-		// add mouse events to the canvas
-		$cvdiv.mousedown(panelApl.cvmsDown);
-		$cvdiv.mouseup(panelApl.cvmsUp);
-		$cvdiv.mouseleave(panelApl.cvmsUp);
-		$cvdiv.mousemove(panelApl.cvmsMove);
-		// add touch events to the canvas
-		$cvdiv.bind("touchstart", panelApl.cvmsDown);
-		$cvdiv.bind("touchend", panelApl.cvmsUp);
-		$cvdiv.bind("touchend", panelApl.cvmsUp);
-		$cvdiv.bind("touchmove", panelApl.cvmsMove);
-		
 		// init canvas
 		panelApl.canv.init();
 		
         panelApl.timer.play();
-		$btn.text('stop');
-    };
-
-    /* mousedown process
-     * {event} evt: event obj
-     * return: none
-     */
-    panelApl.cvmsDown = function(evt) {
-		// convert coordinate from point to canvas
-		var cx = evt.pageX - panelApl.canv.cvpos.x;
-		var cy = evt.pageY - panelApl.canv.cvpos.y;
-		panelApl.drag.now = true;
-		return false;
-    };
-    /* mouseup/mouseleave process
-     * {event} evt: event obj
-     * return: none
-     */
-    panelApl.cvmsUp = function(evt) {
-		if (panelApl.drag.now) {
-			// convert coordinate from point to canvas
-			var cx = evt.pageX - panelApl.canv.cvpos.x;
-			var cy = evt.pageY - panelApl.canv.cvpos.y;
-			if (cx < 0) cx = 0;
-			if (cx > panelApl.canv.area.w) cx = panelApl.canv.area.w;
-			if (cy < 0) cy = 0;
-			if (cy > panelApl.canv.area.h) cy = panelApl.canv.area.h;
-			
-			panelApl.drag.now = false;
-		}
-    };
-    /* mousemove process
-     * {event} evt: evnet obj
-     * return: none
-     */
-    panelApl.cvmsMove = function(evt) {
-		if (panelApl.drag.now) {
-			// convert coordinate from point to canvas
-			var cx = evt.pageX - panelApl.canv.cvpos.x;
-			var cy = evt.pageY - panelApl.canv.cvpos.y;
-			// check if the canvas should be updated
-			var updSep = 1; // #. of pixels that canvas is updated if an object is moved by
-			// update the canvas
-			//	    panelApl.canv.draw({x:cx, y:cy});
-		}
-		return false;
     };
 
     /* body onload process */
@@ -101,17 +36,13 @@ var panelApl = {}; // namespace
         panelApl.timer.set({
             action: function() {
                 panelApl.count ++;
-				//                if (panelApl.count  > 500) {
-				//                    panelApl.timer.pause();
-				//                }
                 panelApl.canv.moveObj();
                 panelApl.canv.draw();
             },
             time: 15
         });
-		//        panelApl.timer.play();
-		panelApl.start();
 
+		panelApl.start();
     });
 
 
