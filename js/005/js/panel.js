eefddb20105b5ced8db97d702cd14ede7f307482
refluster <refluster@gmail.com>
2015-08-16 20:50:10 +0900

    lab/js/005 app refactor

--- js/005/js/panel.js
+++ js/005/js/panel.js
@@ -1,39 +1,26 @@
 var panelApl = {}; // namespace
 
-(function($) {
+$(function() {
     panelApl.timer = $.timer();
 
-    panelApl.start = function() {
-		// init canvas
-		panelApl.canv.init();
-        panelApl.timer.play();
-    };
-
-    /* body onload process */
-    $(window).load(function() {
-		// get canvas's DOM element and context
-		var canvas = document.getElementById('canvas');
-		if ( ! canvas || ! canvas.getContext ) { return false; }
-		var ctx = canvas.getContext("2d");
-		ctx.lineWidth = 1;
-		ctx.globalCompositeOperation = "source-over";
-
-		// display
-		panelApl.canv = new canvasManager.canv(ctx, canvas.width, canvas.height, panelApl);
-		panelApl.canv.init();
-		panelApl.canv.draw();
-
-        panelApl.timer.set({
-            action: function() {
-                panelApl.count ++;
-                panelApl.canv.moveObj();
-                panelApl.canv.draw();
-            },
-            time: 15
-        });
-
-		panelApl.start();
+	// get canvas's DOM element and context
+	var canvas = document.getElementById('canvas');
+	if ( ! canvas || ! canvas.getContext ) { return false; }
+	var ctx = canvas.getContext("2d");
+	ctx.lineWidth = 1;
+	ctx.globalCompositeOperation = "source-over";
+	
+	// display
+	panelApl.canv = new canvasManager.canv(ctx, canvas.width, canvas.height, panelApl);
+	panelApl.canv.draw();
+	
+    panelApl.timer.set({
+        action: function() {
+            panelApl.canv.moveObj();
+            panelApl.canv.draw();
+        },
+        time: 15
     });
-
-
-})(jQuery);
+	
+	panelApl.timer.play();
+});
