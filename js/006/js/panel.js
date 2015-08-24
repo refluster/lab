cbe76320e5662119ea3a27b4d51ebf3becb8ec42
4028188 <refluster@gmail.com>
2015-08-24 19:29:52 +0900

    js/006 merge two js files

--- js/006/js/panel.js
+++ js/006/js/panel.js
@@ -6,9 +6,9 @@ var Apl = function() {
 	// get canvas's DOM element and context
 	var $canvas = $('#canvas');
 	if ( ! $canvas[0] || ! $canvas[0].getContext ) { return false; }
-	var ctx = $canvas[0].getContext("2d");
-	ctx.lineWidth = 1;
-	ctx.globalCompositeOperation = "source-over";
+	this.ctx = $canvas[0].getContext("2d");
+	this.ctx.lineWidth = 1;
+	this.ctx.globalCompositeOperation = "source-over";
 	
     // get canvas info
     this.canvasLeft = $canvas.offset().left;
@@ -17,16 +17,29 @@ var Apl = function() {
     this.canvasHeight = $canvas.attr('height')
 	
 	// display
-	this.canv = new canvasManager(ctx, $canvas[0].width, $canvas[0].height, this);
-	this.canv.draw();
+	this.PI2 = Math.PI * 2; // 2*pi
+	this.radius = 30; // raduis of balls
+	this.holdBallIdx = null; // index of the hold ball (for positioning)
+	this.releasedBallIdx = null; // index of the released ball (for speed setting)
+	this.prevHoldBallPos = {x:0, y:0}; // previous hold ball pos (updated by timer)
+
+	this.ball = [];
+	this.ball[0] = {
+		pos:{x:100, y:100},
+		speed:{x:0, y:0},
+		color:'green',
+		moveNow: false
+	};
+
+	this.draw();
 	
 	// timer
 	this.timer = $.timer();
 	this.timer.set({
 		action: function() {
-			this.canv.moveObj();
-			this.canv.setSpeedMovedObj();
-			this.canv.draw();
+			this.moveObj();
+			this.setSpeedMovedObj();
+			this.draw();
 		}.bind(this),
 		time: 40
 	});
@@ -45,7 +58,6 @@ var Apl = function() {
 	// start timer
 	this.timer.play();
 };
-
 Apl.prototype.getCanvasPosition = function(e) {
     if (e.originalEvent.touches != undefined && e.originalEvent.touches.length > 0) {
         return {x: parseInt(e.originalEvent.touches[0].pageX - this.canvasLeft),
@@ -55,33 +67,117 @@ Apl.prototype.getCanvasPosition = function(e) {
                 y: parseInt(e.pageY - this.canvasTop)};
     }
 };
-
 Apl.prototype.hDown = function(e) {
     var p = this.getCanvasPosition(e);
 	this.dragging = true;
-	this.canv.holdAt(p);
+	this.holdAt(p);
 	return false;
 };
-
 Apl.prototype.hUp = function(e) {
 	if (this.dragging) {
 		var p = this.getCanvasPosition(e);
 		if (p.x < 0) p.x = 0;
-		if (p.x > this.canv.area.w) p.x = this.canv.area.w;
+		if (p.x > this.canvasWidth) p.x = this.canvasWidth;
 		if (p.y < 0) p.y = 0;
-		if (p.y > this.canv.area.h) p.y = this.canv.area.h;
+		if (p.y > this.canvasHeight) p.y = this.canvasHeight;
 
 		this.dragging = false;
-		this.canv.releaseAt(p);
+		this.releaseAt(p);
 	}
 };
-
 Apl.prototype.hMove = function(e) {
 	if (this.dragging) {
 		var p = this.getCanvasPosition(e);
-		this.canv.moveTo(p);
+		this.moveTo(p);
 	}
 	return false;
+};
+Apl.prototype._blank = function() {
+	this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+};
+Apl.prototype.draw = function() {
+	this._blank();
+	this.ctx.strokeStyle = this.color;
+	this.ctx.globalAlpha = 0.5;
+
+	for (var i = 0; i < this.ball.length; i++) {
+		this.ctx.fillStyle = this.ball[i].color;
+		this.ctx.beginPath();
+		this.ctx.arc(this.ball[i].pos.x, this.ball[i].pos.y,
+					 this.radius, 0, this.PI2, false);
+		this.ctx.fill();
+	}
+};
+Apl.prototype.moveObj = function() {
+	for (var i = 0; i < this.ball.length; i++) {
+		if (this.ball[i].moveNow) {
+			this.ball[i].pos.x += this.ball[i].speed.x;
+			this.ball[i].pos.y += this.ball[i].speed.y;
+			if (this.ball[i].pos.x + this.radius > this.canvasWidth) {
+				this.ball[i].pos.x = this.canvasWidth -
+					(this.ball[i].pos.x + this.radius - this.canvasWidth) - this.radius;
+				this.ball[i].speed.x = -this.ball[i].speed.x;
+			}
+			if (this.ball[i].pos.x - this.radius < 0) {
+				this.ball[i].pos.x = -(this.ball[i].pos.x - this.radius) + this.radius;
+				this.ball[i].speed.x = -this.ball[i].speed.x;
+			}
+			if (this.ball[i].pos.y + this.radius > this.canvasHeight) {
+				this.ball[i].pos.y = this.canvasHeight
+					- (this.ball[i].pos.y + this.radius - this.canvasHeight) - this.radius;
+				this.ball[i].speed.y = -this.ball[i].speed.y;
+			}
+			if (this.ball[i].pos.y - this.radius < 0) {
+				this.ball[i].pos.y = -(this.ball[i].pos.y - this.radius) + this.radius;
+				this.ball[i].speed.y = -this.ball[i].speed.y;
+			}
+		}
+	}
+};
+Apl.prototype.setSpeedMovedObj = function() {
+	if (this.releasedBallIdx != null) {
+		this.ball[this.releasedBallIdx].speed.x =
+			this.ball[this.releasedBallIdx].pos.x - this.prevHoldBallPos.x;
+		this.ball[this.releasedBallIdx].speed.y =
+			this.ball[this.releasedBallIdx].pos.y - this.prevHoldBallPos.y;
+
+		if (this.ball[this.releasedBallIdx].speed.x != 0 ||
+			this.ball[this.releasedBallIdx].speed.y != 0) {
+			this.ball[this.releasedBallIdx].moveNow = true;
+			console.log('moveing (speed)');
+		}
+		this.releasedBallIdx = null;
+	}
+	if (this.holdBallIdx != null) {
+		this.prevHoldBallPos.x = this.ball[this.holdBallIdx].pos.x;
+		this.prevHoldBallPos.y = this.ball[this.holdBallIdx].pos.y;
+	}
+}
+Apl.prototype.holdAt = function(pos) {
+	// check if the clicked position is on a ball
+	for (var i = 0; i < this.ball.length; i++) {
+		if (this.ball[i].pos.x - this.radius < pos.x &&
+			this.ball[i].pos.x + this.radius > pos.x &&
+			this.ball[i].pos.y - this.radius < pos.y &&
+			this.ball[i].pos.y + this.radius > pos.y) {
+			this.holdBallIdx = i;
+			this.prevCursorPos = pos;
+			this.ball[i].moveNow = false;
+		}
+	}
+};
+Apl.prototype.releaseAt = function(pos) {
+	if (this.holdBallIdx != null) {
+		this.releasedBallIdx = this.holdBallIdx;
+		this.holdBallIdx = null;
+	}
+};
+Apl.prototype.moveTo = function(pos) {
+	if (this.holdBallIdx != null) {
+		this.ball[this.holdBallIdx].pos.x += pos.x - this.prevCursorPos.x;
+		this.ball[this.holdBallIdx].pos.y += pos.y - this.prevCursorPos.y;
+		this.prevCursorPos = pos;
+	}
 };
 
 $(function() {
