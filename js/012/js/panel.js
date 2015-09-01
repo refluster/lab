1950584a9ed31273cc0d89a6dac7573103e5182e
refluster <refluster@gmail.com>
2015-09-02 08:54:23 +0900

    js/012 make class

--- js/012/js/panel.js
+++ js/012/js/panel.js
@@ -1,76 +1,74 @@
-var panelApl = {};
+var panelApl = function() {
+	this.width;
+	this.height;
+	this.renderer;
+	this.camera;
+	this.scene;
+	this.light;
+	this.cube;
+	this.baseTime = +new Date;
+};
 
-(function($) {
-	panelApl.width;
-	panelApl.height;
-	panelApl.renderer;
-	panelApl.camera;
-	panelApl.scene;
-	panelApl.light;
-	panelApl.cube;
+panelApl.prototype.initThree = function() {
+		this.width = document.getElementById('canvas-frame').clientWidth;
+		this.height = document.getElementById('canvas-frame').clientHeight;
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setSize(this.width, this.height );
+		document.getElementById('canvas-frame').appendChild(this.renderer.domElement);
+		this.renderer.setClearColorHex(0x000000, 1.0);
+};
 
-	panelApl.initThree = function() {
-		panelApl.width = document.getElementById('canvas-frame').clientWidth;
-		panelApl.height = document.getElementById('canvas-frame').clientHeight;
-		panelApl.renderer = new THREE.WebGLRenderer({antialias: true});
-		panelApl.renderer.setSize(panelApl.width, panelApl.height );
-		document.getElementById('canvas-frame').appendChild(panelApl.renderer.domElement);
-		panelApl.renderer.setClearColorHex(0x000000, 1.0);
-	};
-
-	panelApl.initCamera = function() {
-		panelApl.camera = new THREE.PerspectiveCamera( 15 , panelApl.width / panelApl.height,
+panelApl.prototype.initCamera = function() {
+		this.camera = new THREE.PerspectiveCamera( 15 , this.width / this.height,
 													   1 , 10000 );
-		panelApl.camera.position.x = 0;
-		panelApl.camera.position.y = 40;
-		panelApl.camera.position.z = 80;
-		panelApl.camera.lookAt( {x:0, y:1, z:0 } );
-	};
+		this.camera.position.x = 0;
+		this.camera.position.y = 40;
+		this.camera.position.z = 80;
+		this.camera.lookAt( {x:0, y:1, z:0 } );
+};
 
-	panelApl.initScene = function() {
-		panelApl.scene = new THREE.Scene();
-	};
+panelApl.prototype.initScene = function() {
+		this.scene = new THREE.Scene();
+};
 
-	panelApl.initLight = function() {
-		panelApl.light = new THREE.DirectionalLight(0xcccccc, 1.0, 0);
-		panelApl.light.position.set( 0.577, 0.577, 0.577 );
-		panelApl.ambient = new THREE.AmbientLight(0x333333);
+panelApl.prototype.initLight = function() {
+		this.light = new THREE.DirectionalLight(0xcccccc, 1.0, 0);
+		this.light.position.set( 0.577, 0.577, 0.577 );
+		this.ambient = new THREE.AmbientLight(0x333333);
 
-		panelApl.scene.add(panelApl.light);
-		panelApl.scene.add(panelApl.ambient);
-	};
+		this.scene.add(this.light);
+		this.scene.add(this.ambient);
+};
 
-	panelApl.initObject = function(){
-		panelApl.mesh = new THREE.Object3D();
+panelApl.prototype.initObject = function(){
+		this.mesh = new THREE.Object3D();
 		var loader = new THREE.JSONLoader();
 		loader.load('./js/monkey.js', function(geometry) {
-			panelApl.mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial);
-			panelApl.mesh = new THREE.Mesh(geometry,
+			this.mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial);
+			this.mesh = new THREE.Mesh(geometry,
 										   new THREE.MeshLambertMaterial({color: 0x444444}));
-			panelApl.mesh.scale = new THREE.Vector3(2, 2, 2);
-			panelApl.scene.add(panelApl.mesh);
-		});
-	};
+			this.mesh.scale = new THREE.Vector3(2, 2, 2);
+			this.scene.add(this.mesh);
+		}.bind(this));
+};
 
-	panelApl.baseTime = +new Date;
-	panelApl.render = function() {
-		requestAnimationFrame(panelApl.render);
-		panelApl.mesh.rotation.y = 0.3 * (+new Date - panelApl.baseTime) / 1000;
-		panelApl.renderer.render(panelApl.scene, panelApl.camera);
-	};
+panelApl.prototype.render = function() {
+	requestAnimationFrame(this.render.bind(this));
+		this.mesh.rotation.y = 0.3 * (+new Date - this.baseTime) / 1000;
+		this.renderer.render(this.scene, this.camera);
+};
 
-	panelApl.threeStart = function() {
-		panelApl.initThree();
-		panelApl.initCamera();
-		panelApl.initScene();
-		panelApl.initLight();
-		panelApl.initObject();
-		panelApl.renderer.clear();
-		panelApl.render();
-	}
+panelApl.prototype.threeStart = function() {
+		this.initThree();
+		this.initCamera();
+		this.initScene();
+		this.initLight();
+		this.initObject();
+		this.renderer.clear();
+		this.render();
+};
 
-	$(window).load(function() {
-		panelApl.threeStart();
-	});
-
-})(jQuery);
+$(function() {
+    var apl = new panelApl();
+	apl.threeStart();
+});
