0b283405ead7fb4479ff77cc7c79d6a482d62ba9
refluster <refluster@gmail.com>
2015-09-05 22:57:08 +0900

    js/014 indent panel.js

--- js/014/js/panel.js
+++ js/014/js/panel.js
@@ -30,57 +30,57 @@ var panelApl = function() {
 };
 
 panelApl.prototype.start = function() {
-		var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
-		// add mouse events to the canvas
+	var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
+	// add mouse events to the canvas
 	$cvdiv.mousedown(this.cvmsDown.bind(this));
-		$cvdiv.mouseup(this.cvmsUp.bind(this));
-		$cvdiv.mouseleave(this.cvmsUp.bind(this));
-		$cvdiv.mousemove(this.cvmsMove.bind(this));
-		// add touch events to the canvas
-		$cvdiv.bind("touchstart", this.cvmsDown.bind(this));
-		$cvdiv.bind("touchend", this.cvmsUp.bind(this));
-		$cvdiv.bind("touchend", this.cvmsUp.bind(this));
-		$cvdiv.bind("touchmove", this.cvmsMove.bind(this));
-		
-		// init canvas
-		this.canv.init();
+	$cvdiv.mouseup(this.cvmsUp.bind(this));
+	$cvdiv.mouseleave(this.cvmsUp.bind(this));
+	$cvdiv.mousemove(this.cvmsMove.bind(this));
+	// add touch events to the canvas
+	$cvdiv.bind("touchstart", this.cvmsDown.bind(this));
+	$cvdiv.bind("touchend", this.cvmsUp.bind(this));
+	$cvdiv.bind("touchend", this.cvmsUp.bind(this));
+	$cvdiv.bind("touchmove", this.cvmsMove.bind(this));
+	
+	// init canvas
+	this.canv.init();
 
-		// init timer
-		this.timer.play();
+	// init timer
+	this.timer.play();
 };
 
 panelApl.prototype.cvmsDown = function(evt) {
+	// convert coordinate from point to canvas
+	var cx = evt.pageX - this.canv.cvpos.x;
+	var cy = evt.pageY - this.canv.cvpos.y;
+	this.dragging = true;
+	return false;
+};
+panelApl.prototype.cvmsUp = function(evt) {
+	if (this.dragging == true) {
 		// convert coordinate from point to canvas
 		var cx = evt.pageX - this.canv.cvpos.x;
 		var cy = evt.pageY - this.canv.cvpos.y;
-		this.dragging = true;
-		return false;
-};
-panelApl.prototype.cvmsUp = function(evt) {
-		if (this.dragging == true) {
-			// convert coordinate from point to canvas
-			var cx = evt.pageX - this.canv.cvpos.x;
-			var cy = evt.pageY - this.canv.cvpos.y;
-			if (cx < 0) cx = 0;
-			if (cx > this.canv.area.w) cx = this.canv.area.w;
-			if (cy < 0) cy = 0;
-			if (cy > this.canv.area.h) cy = this.canv.area.h;
+		if (cx < 0) cx = 0;
+		if (cx > this.canv.area.w) cx = this.canv.area.w;
+		if (cy < 0) cy = 0;
+		if (cy > this.canv.area.h) cy = this.canv.area.h;
 
-			this.dragging = false;
-		}
+		this.dragging = false;
+	}
 };
 panelApl.prototype.cvmsMove = function(evt) {
-		if (this.dragging == true) {
-			// convert coordinate from point to canvas
-			var cx = evt.pageX - this.canv.cvpos.x;
-			var cy = evt.pageY - this.canv.cvpos.y;
-			// check if the canvas should be updated
-			var updSep = 1; // #. of pixels that canvas is updated if an object is moved by
-			// update the canvas
-			this.canv.moveTo({x:cx, y:cy});
-			this.timer.play();
-		}
-		return false;
+	if (this.dragging == true) {
+		// convert coordinate from point to canvas
+		var cx = evt.pageX - this.canv.cvpos.x;
+		var cy = evt.pageY - this.canv.cvpos.y;
+		// check if the canvas should be updated
+		var updSep = 1; // #. of pixels that canvas is updated if an object is moved by
+		// update the canvas
+		this.canv.moveTo({x:cx, y:cy});
+		this.timer.play();
+	}
+	return false;
 };
 
 $(function() {
