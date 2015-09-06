25e1512451c46bb6e71de90e92d42386f7da15a2
refluster <refluster@gmail.com>
2015-09-06 22:12:42 +0900

    js/026 indent code

--- js/026/js/canvas.js
+++ js/026/js/canvas.js
@@ -1,68 +1,68 @@
 var canvasManager = {}; // namespace
 
 (function($) {
-    canvasManager.canv = function(ctx, w, h) {
-	this.ctx = ctx; // the context
-	this.area = {w:w, h:h};  // the area
-	this.cvpos = {x:0, y:0};  // position of the canvas on the browser
-        this.prevPos = {x:0, y:0}; // previous position of the cursor
-        this.lineWidth = 1;
-        this.PI2 = Math.PI * 2; // 2*pi
-        
-	// set the position of the canvas on the browser
-	var $cvdiv = $('#cvdiv1');
-	this.cvpos.x = $cvdiv.offset().left;
-	this.cvpos.y = $cvdiv.offset().top;
+	canvasManager.canv = function(ctx, w, h) {
+		this.ctx = ctx; // the context
+		this.area = {w:w, h:h};  // the area
+		this.cvpos = {x:0, y:0};  // position of the canvas on the browser
+		this.prevPos = {x:0, y:0}; // previous position of the cursor
+		this.lineWidth = 1;
+		this.PI2 = Math.PI * 2; // 2*pi
 
-        // environment parameter
-        this.drawInterval = 33; //msec
-        this.radius = 1; // raduis of balls
-        this.particles = [];
-        this.sph;
+		// set the position of the canvas on the browser
+		var $cvdiv = $('#cvdiv1');
+		this.cvpos.x = $cvdiv.offset().left;
+		this.cvpos.y = $cvdiv.offset().top;
 
-	this.ctx.lineWidth = 1;
-	this.ctx.globalCompositeOperation = "source-over";
+		// environment parameter
+		this.drawInterval = 33; //msec
+		this.radius = 1; // raduis of balls
+		this.particles = [];
+		this.sph;
 
-	this.blank = function() {
-	    //this.ctx.clearRect(0, 0, this.area.w, this.area.h);
-            this.ctx.fillStyle = 'black';
-            this.ctx.fillRect(0, 0, this.area.w, this.area.h);
-	};
-        
-        this.init = function() {
-            this.sph = new Sph();
-            this.sph.init();
-        };
-        
-        this.setFps = function(fps) {
-            this.drawInterval = 1000/fps;
-        };
+		this.ctx.lineWidth = 1;
+		this.ctx.globalCompositeOperation = "source-over";
 
-        this.moveObj = function() {
-            this.sph.step();
-        };
+		this.blank = function() {
+			//this.ctx.clearRect(0, 0, this.area.w, this.area.h);
+			this.ctx.fillStyle = 'black';
+			this.ctx.fillRect(0, 0, this.area.w, this.area.h);
+		};
 
-        this.draw = function() {
-            this.blank();
-            this.ctx.save();
+		this.init = function() {
+			this.sph = new Sph();
+			this.sph.init();
+		};
 
-            // draw ball
-            this.ctx.fillStyle = 'white';
-            //this.ctx.globalAlpha = 0.5;
+		this.setFps = function(fps) {
+			this.drawInterval = 1000/fps;
+		};
 
-            var p = this.sph.get_particle();
+		this.moveObj = function() {
+			this.sph.step();
+		};
 
-            for (var i = 0; i < p.length; i++) {
-                this.ctx.beginPath();
-                this.ctx.arc(p[i].pos.x*8, this.area.h-p[i].pos.y*8,
-                             this.radius, 0, this.PI2, false);
-                this.ctx.fill();
-            }
+		this.draw = function() {
+			this.blank();
+			this.ctx.save();
 
-            this.ctx.restore();
-	};
+			// draw ball
+			this.ctx.fillStyle = 'white';
+			//this.ctx.globalAlpha = 0.5;
 
-    }
+			var p = this.sph.get_particle();
+
+			for (var i = 0; i < p.length; i++) {
+				this.ctx.beginPath();
+				this.ctx.arc(p[i].pos.x*8, this.area.h-p[i].pos.y*8,
+							 this.radius, 0, this.PI2, false);
+				this.ctx.fill();
+			}
+
+			this.ctx.restore();
+		};
+
+	}
 
 })(jQuery);
 
--- js/026/js/panel.js
+++ js/026/js/panel.js
@@ -4,66 +4,66 @@
 var panelApl = {}; // namespace
 
 (function($) {
-    panelApl.start = false;  // true if playing
-    panelApl.timer = $.timer();
-    panelApl.fps = 30;
-    
-    /* button process
-     * return: none
-     */
-    panelApl.start = function() {
-	var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
-	var $btn = $('#stbtn1'); // start button
-        
-	if (!panelApl.start) { // if not playing
-	    // init canvas
-	    panelApl.canv.init();
-	    panelApl.start = true;
-            panelApl.canv.setFps(panelApl.fps);
-	    panelApl.showmsg('moving');
-            panelApl.timer.play();
-	    $btn.text('stop');
-	} else { // if playing
-	    panelApl.start = false;
-            panelApl.timer.pause();
-	    panelApl.showmsg('paused');
-	    $btn.text('start');
-	}
-    };
+	panelApl.start = false;  // true if playing
+	panelApl.timer = $.timer();
+	panelApl.fps = 30;
 
-    panelApl.showmsg = function(msg) {
-	$('#msg1').html(msg);
-    };
+	/* button process
+	 * return: none
+	 */
+	panelApl.start = function() {
+		var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
+		var $btn = $('#stbtn1'); // start button
 
-    /* body onload process */
-    $(window).load(function() {
-	// get canvas's DOM element and context
-	var canvas = document.getElementById('cv1');
-	if ( ! canvas || ! canvas.getContext ) { return false; }
-	var ctx = canvas.getContext("2d");
-        
-        // canvas
-	panelApl.canv = new canvasManager.canv(ctx, canvas.width,
-                                               canvas.height);
-	panelApl.canv.init();
-	panelApl.canv.draw();
-        
-	// set events
-	var $btn = $('#stbtn1'); // start button
-	$btn.mousedown(panelApl.start);
-	$btn.text('start');
-	
-	// show message
-	panelApl.showmsg('press start button');
-        window.addEventListener('devicemotion', panelApl.readGravity);
-        
-        panelApl.timer.set({
-            action: function() {
-                panelApl.canv.moveObj();
-                panelApl.canv.draw();
-            },
-            time: 1000/panelApl.fps
-        });
-        
-    });
+		if (!panelApl.start) { // if not playing
+			// init canvas
+			panelApl.canv.init();
+			panelApl.start = true;
+			panelApl.canv.setFps(panelApl.fps);
+			panelApl.showmsg('moving');
+			panelApl.timer.play();
+			$btn.text('stop');
+		} else { // if playing
+			panelApl.start = false;
+			panelApl.timer.pause();
+			panelApl.showmsg('paused');
+			$btn.text('start');
+		}
+	};
+
+	panelApl.showmsg = function(msg) {
+		$('#msg1').html(msg);
+	};
+
+	/* body onload process */
+	$(window).load(function() {
+		// get canvas's DOM element and context
+		var canvas = document.getElementById('cv1');
+		if ( ! canvas || ! canvas.getContext ) { return false; }
+		var ctx = canvas.getContext("2d");
+
+		// canvas
+		panelApl.canv = new canvasManager.canv(ctx, canvas.width,
+											   canvas.height);
+		panelApl.canv.init();
+		panelApl.canv.draw();
+
+		// set events
+		var $btn = $('#stbtn1'); // start button
+		$btn.mousedown(panelApl.start);
+		$btn.text('start');
+
+		// show message
+		panelApl.showmsg('press start button');
+		window.addEventListener('devicemotion', panelApl.readGravity);
+
+		panelApl.timer.set({
+			action: function() {
+				panelApl.canv.moveObj();
+				panelApl.canv.draw();
+			},
+			time: 1000/panelApl.fps
+		});
+
+	});
 })(jQuery);
