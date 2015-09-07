556cde3efb7cccf69d41aa3cdd2ba3610b1d402b
refluster <refluster@gmail.com>
2015-09-08 08:19:38 +0900

    js/027 indent html, js

--- js/027/js/panel.js
+++ js/027/js/panel.js
@@ -4,71 +4,71 @@
 var panelApl = {}; // namespace
 
 (function($) {
-    panelApl.start = false;  // true if playing
-    panelApl.timer = $.timer();
-    panelApl.fps = 30;
-    
-    /* button process
-     * return: none
-     */
-    panelApl.start = function() {
-	var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
-	var $btn = $('#stbtn1'); // start button
-        
-	if (!panelApl.start) { // if not playing
-	    // init canvas
-	    panelApl.canv.init();
-	    panelApl.start = true;
-            panelApl.canv.setFps(panelApl.fps);
-	    panelApl.showmsg('moving');
-            panelApl.timer.play();
-	    $btn.text('stop');
-	} else { // if playing
-	    panelApl.start = false;
-            panelApl.timer.pause();
-	    panelApl.showmsg('paused');
-	    $btn.text('start');
-	}
-    };
+	panelApl.start = false;  // true if playing
+	panelApl.timer = $.timer();
+	panelApl.fps = 30;
 
-    panelApl.showmsg = function(msg) {
-	$('#msg1').html(msg);
-    };
+	/* button process
+	 * return: none
+	 */
+	panelApl.start = function() {
+		var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
+		var $btn = $('#stbtn1'); // start button
 
-    panelApl.getGravity = function(evt) {
-        var acg = evt.accelerationIncludingGravity;
-        panelApl.canv.setGravity(acg.x, acg.y, acg.z);
-    };
+		if (!panelApl.start) { // if not playing
+			// init canvas
+			panelApl.canv.init();
+			panelApl.start = true;
+			panelApl.canv.setFps(panelApl.fps);
+			panelApl.showmsg('moving');
+			panelApl.timer.play();
+			$btn.text('stop');
+		} else { // if playing
+			panelApl.start = false;
+			panelApl.timer.pause();
+			panelApl.showmsg('paused');
+			$btn.text('start');
+		}
+	};
 
-    /* body onload process */
-    $(window).load(function() {
-	// get canvas's DOM element and context
-	var canvas = document.getElementById('cv1');
-	if ( ! canvas || ! canvas.getContext ) { return false; }
-	var ctx = canvas.getContext("2d");
-        
-        // canvas
-	panelApl.canv = new canvasManager.canv(ctx, canvas.width,
-                                               canvas.height);
-	panelApl.canv.init();
-	panelApl.canv.draw();
-        
-	// set events
-	var $btn = $('#stbtn1'); // start button
-	$btn.mousedown(panelApl.start);
-	$btn.text('start');
-        window.addEventListener('devicemotion', panelApl.getGravity);
-        	
-	// show message
-	panelApl.showmsg('press start button');
-        
-        panelApl.timer.set({
-            action: function() {
-                panelApl.canv.moveObj();
-                panelApl.canv.draw();
-            },
-            time: 1000/panelApl.fps
-        });
-        
-    });
+	panelApl.showmsg = function(msg) {
+		$('#msg1').html(msg);
+	};
+
+	panelApl.getGravity = function(evt) {
+		var acg = evt.accelerationIncludingGravity;
+		panelApl.canv.setGravity(acg.x, acg.y, acg.z);
+	};
+
+	/* body onload process */
+	$(window).load(function() {
+		// get canvas's DOM element and context
+		var canvas = document.getElementById('cv1');
+		if ( ! canvas || ! canvas.getContext ) { return false; }
+		var ctx = canvas.getContext("2d");
+
+		// canvas
+		panelApl.canv = new canvasManager.canv(ctx, canvas.width,
+											   canvas.height);
+		panelApl.canv.init();
+		panelApl.canv.draw();
+
+		// set events
+		var $btn = $('#stbtn1'); // start button
+		$btn.mousedown(panelApl.start);
+		$btn.text('start');
+		window.addEventListener('devicemotion', panelApl.getGravity);
+
+		// show message
+		panelApl.showmsg('press start button');
+
+		panelApl.timer.set({
+			action: function() {
+				panelApl.canv.moveObj();
+				panelApl.canv.draw();
+			},
+			time: 1000/panelApl.fps
+		});
+
+	});
 })(jQuery);
