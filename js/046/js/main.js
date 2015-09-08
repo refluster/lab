6c1f158bc1cb3f96bddc7056beee175adf3f9d1d
refluster <refluster@gmail.com>
2015-09-08 20:55:17 +0900

    js/046 simple fractal

--- js/046/js/main.js
+++ js/046/js/main.js
@@ -41,17 +41,64 @@ Apl.prototype.initLight = function() {
 };
 
 Apl.prototype.initObject = function(){
+	var material = new THREE.LineBasicMaterial({
+		color: 0xaaaaff
+	});
+	var v3origin = new THREE.Vector3( 0, 0, 0 );
+
+	var addObjects = function(step, root, size, p, direction) {
+		if (step == 3) {
+			return;
+		}
+
+		{
+			var geometry = new THREE.Geometry();
+			var v = new THREE.Vector3( 0, 0, 80*size)
+			geometry.vertices.push(v3origin, v);
+			var line = new THREE.Line( geometry, material );
+			line.position.set(p.x, p.y, p.z);
+			root.add(line);
+			addObjects(step + 1, line, size * 0.8, v, direction);
+		}
+		{
+			var geometry = new THREE.Geometry();
+			var v = new THREE.Vector3( 0, -20*size, -10*size )
+			geometry.vertices.push(v3origin, v);
+			var line = new THREE.Line( geometry, material );
+			line.position.set(p.x, p.y, p.z);
+			root.add(line);
+			addObjects(step + 1, line, size * 0.8, v, direction);
+		}
+	};
+
+	var obj = new THREE.Object3D();
+	addObjects(0, obj, 0.2, {x: 0, y: 0, z: 0}, {x: 0, y: 1, z: 0});
+	
+	this.scene.add(obj);
+
+/*
+	var geometry = new THREE.Geometry();
+	geometry.vertices.push(
+		new THREE.Vector3( 0, 0, 0 ),
+		new THREE.Vector3( 0, 80, 0 )
+	);
+	var line = new THREE.Line( geometry, material );
+	this.scene.add(line);
+*/
+
+/*
 	this.cube = new THREE.Mesh(
 		new THREE.BoxGeometry(50,50,50), // set model
 		new THREE.MeshLambertMaterial({color: 0xff0000}) // set material
 	);
 	this.scene.add(this.cube);
 	this.cube.position.set(0,0,0);
+*/
 };
 
 Apl.prototype.render = function() {
 	requestAnimationFrame(this.render.bind(this));
-	this.cube.rotation.z = 0.5*(+new Date - this.baseTime)/1000;
+//	this.cube.rotation.z = 0.5*(+new Date - this.baseTime)/1000;
 	this.renderer.render(this.scene, this.camera);
 };
 
