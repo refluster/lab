ec6c4418af2d8152c76caf1f77c558fe0dd191a5
refluster <refluster@gmail.com>
2015-09-10 15:12:15 +0900

    js/016 indent canvasManager

--- js/016/js/canvas.js
+++ js/016/js/canvas.js
@@ -1,83 +1,83 @@
 /* HTML5 Canvas drag&drop */
 var canvasManager = function(ctx, w, h, names) {
-		this.ctx = ctx; // the context
-		this.area = {w:w, h:h};  // the area
-		this.cvpos = {x:0, y:0};  // position of the canvas on the browser
+	this.ctx = ctx; // the context
+	this.area = {w:w, h:h};  // the area
+	this.cvpos = {x:0, y:0};  // position of the canvas on the browser
 
-		// set the position of the canvas on the browser
-		var $cvdiv = $('#canvas');
-		this.cvpos.x = $cvdiv.offset().left;
-		this.cvpos.y = $cvdiv.offset().top;
+	// set the position of the canvas on the browser
+	var $cvdiv = $('#canvas');
+	this.cvpos.x = $cvdiv.offset().left;
+	this.cvpos.y = $cvdiv.offset().top;
 
-		this.color = 'white'
-		this.lineWidth = 1;
-		this.PI2 = Math.PI * 2; // 2*pi
-		this.radius = 30; // raduis of balls
+	this.color = 'white'
+	this.lineWidth = 1;
+	this.PI2 = Math.PI * 2; // 2*pi
+	this.radius = 30; // raduis of balls
 
-		this.leaf = [];
+	this.leaf = [];
 
 };
 
 canvasManager.prototype.blank = function() {
-			// clear
-			this.ctx.fillStyle = 'black';
-			//this.ctx.clearRect(0, 0, this.area.w, this.area.h);
-			this.ctx.fillRect(0, 0, this.area.w, this.area.h);
+	// clear
+	this.ctx.fillStyle = 'black';
+	//this.ctx.clearRect(0, 0, this.area.w, this.area.h);
+	this.ctx.fillRect(0, 0, this.area.w, this.area.h);
 };
 
 canvasManager.prototype.init = function() {
-			this.blank();
-			this.clearLeaf();
+	this.blank();
+	this.clearLeaf();
 };
 
 canvasManager.prototype.clearLeaf = function() {
-			this.leaf = [];
+	this.leaf = [];
 };
 
 canvasManager.prototype.addLeaf = function(number, rmin, rmax) {
-			for (var i = 0; i < number; i++) {
-				var rand = Math.floor(Math.random()*1024*1024);
-				var x = rand%(this.area.w - 1);
-				var y = rand%this.area.h;
-				var r = rmin + rand%(rmax - rmin);
+	for (var i = 0; i < number; i++) {
+		var rand = Math.floor(Math.random()*1024*1024);
+		var x = rand%(this.area.w - 1);
+		var y = rand%this.area.h;
+		var r = rmin + rand%(rmax - rmin);
 
-				this.leaf.push({
-					pos:{x:x, y:y},
-					radius:r
-				});
-			}
+		this.leaf.push({
+			pos:{x:x, y:y},
+			radius:r
+		});
+	}
 };
 
-		/* draw canvas
-		 * return: none
-		 */
+/* draw canvas
+ * return: none
+ */
 canvasManager.prototype.draw = function() {
-			var sin5 = []
-			var cos5 = [];
+	var sin5 = []
+	var cos5 = [];
 
-			for (var i = 0; i < 5; i++) {
-				sin5[i] = Math.sin(i*Math.PI/5);
-				cos5[i] = Math.cos(i*Math.PI/5);
-			}
+	for (var i = 0; i < 5; i++) {
+		sin5[i] = Math.sin(i*Math.PI/5);
+		cos5[i] = Math.cos(i*Math.PI/5);
+	}
 
-			this.ctx.save();
-			this.ctx.strokeStyle = this.color;
-			this.ctx.globalAlpha = 0.5;
+	this.ctx.save();
+	this.ctx.strokeStyle = this.color;
+	this.ctx.globalAlpha = 0.5;
 
-			for (var i = 0; i < this.leaf.length; i++) {
-				var l = this.leaf[i];
-				this.ctx.beginPath();
+	for (var i = 0; i < this.leaf.length; i++) {
+		var l = this.leaf[i];
+		this.ctx.beginPath();
 
-				for (var j = 0; j < cos5.length; j++) {
-					this.ctx.moveTo(l.pos.x + Math.floor(l.radius*cos5[j]),
-									l.pos.y + Math.floor(l.radius*sin5[j]));
-					this.ctx.lineTo(l.pos.x - Math.floor(l.radius*cos5[j]),
-									l.pos.y - Math.floor(l.radius*sin5[j]));
-				}
+		for (var j = 0; j < cos5.length; j++) {
+			this.ctx.moveTo(l.pos.x + Math.floor(l.radius*cos5[j]),
+							l.pos.y + Math.floor(l.radius*sin5[j]));
+			this.ctx.lineTo(l.pos.x - Math.floor(l.radius*cos5[j]),
+							l.pos.y - Math.floor(l.radius*sin5[j]));
+		}
 
-				this.ctx.stroke();
+		this.ctx.stroke();
 
-			}
+	}
 
-			this.ctx.restore();
+	this.ctx.restore();
 };
