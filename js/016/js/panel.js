977f4c0210c7b9dce4fecf75ba49f6ea38eb1def
refluster <refluster@gmail.com>
2015-09-10 14:41:12 +0900

    js/016 make class of panelApl

--- js/016/js/panel.js
+++ js/016/js/panel.js
@@ -1,18 +1,37 @@
 /* HTML5 Canvas drag&drop
  * canvas is updated when an object is dragged by 1px
  */
-var panelApl = {}; // namespace
-
-(function($) {
+var panelApl = function() {
 	// display message
 	// {string} msg: displayed message
 	// return: none
-	panelApl.text = $("#txt1");
-	panelApl.number; // the number of leaves
-	panelApl.rmin; // min radius of leaves
-	panelApl.rmax; // max radius of leaves
+	this.text = $("#txt1");
+	this.number; // the number of leaves
+	this.rmin; // min radius of leaves
+	this.rmax; // max radius of leaves
 
-	panelApl.update = function() {
+		console.log("window.load");
+
+		// get canvas's DOM element and context
+		var canvas = document.getElementById('canvas');
+		if ( ! canvas || ! canvas.getContext ) { return false; }
+		var ctx = canvas.getContext("2d");
+		ctx.globalCompositeOperation = "source-over";
+
+		// display
+		this.canv = new canvasManager.canv(ctx, canvas.width,
+											   canvas.height, this);
+		// set event
+		var $btn = $('#btn1'); // start button
+		$btn.mousedown(this.update.bind(this));
+		$btn.text("update");
+
+		setTimeout(this.update.bind(this), 1000);
+
+		// update by initial parameter
+};
+
+panelApl.prototype.update = function() {
 		var number = document.form1.input_number.value;
 		var rmin = document.form1.input_rmin.value;
 		var rmax = document.form1.input_rmax.value;
@@ -22,7 +41,7 @@ var panelApl = {}; // namespace
 		// check if inputs are number
 		if (isNaN(number) || isNaN(rmin) || isNaN(rmax) ||
 			isNaN(seconds) || isNaN(fps)) {
-			//panelApl.showmsg("incorrect input");
+			//this.showmsg("incorrect input");
 			return;
 		}
 
@@ -33,64 +52,43 @@ var panelApl = {}; // namespace
 		fps = Number(fps);
 
 		if (number < 0 || rmin < 0 || rmax < 0 || seconds < 0 || fps < 0) {
-			//panelApl.showmsg("values must be positive");
+			//this.showmsg("values must be positive");
 			return;
 		}
 
 		if (rmin > rmax) {
-			//panelApl.showmsg("rmax must be larger than rmin");
+			//this.showmsg("rmax must be larger than rmin");
 			return;
 		}
 
-		panelApl.number = number;
-		panelApl.rmin = rmin;
-		panelApl.rmax = rmax;
+		this.number = number;
+		this.rmin = rmin;
+		this.rmax = rmax;
 
-		panelApl.canv.init();
+		this.canv.init();
 
-		//panelApl.canv.addLeaf(number, rmin, rmax);
-		//panelApl.canv.draw();
+		//this.canv.addLeaf(number, rmin, rmax);
+		//this.canv.draw();
 
 		var curNumber = 0;
 		var incNumber = number/(seconds*fps);
 
 		var addAndDraw = function() {
-			panelApl.canv.clearLeaf();
+			this.canv.clearLeaf();
 			if (curNumber + incNumber > number) {
-				panelApl.canv.addLeaf(number - curNumber, rmin, rmax);
+				this.canv.addLeaf(number - curNumber, rmin, rmax);
 			} else {
 				setTimeout(addAndDraw, 1000/fps);
-				panelApl.canv.addLeaf(incNumber, rmin, rmax);
+				this.canv.addLeaf(incNumber, rmin, rmax);
 				curNumber += incNumber;
 			}
-			panelApl.canv.draw();
-		};
+			this.canv.draw();
+		}.bind(this);
 
 		var timer = setTimeout(addAndDraw, 50);//1000/fps);
-	};
+};
 
-	/* body onload process */
-	$(window).load(function() {
-		console.log("window.load");
-
-		// get canvas's DOM element and context
-		var canvas = document.getElementById('canvas');
-		if ( ! canvas || ! canvas.getContext ) { return false; }
-		var ctx = canvas.getContext("2d");
-		ctx.globalCompositeOperation = "source-over";
-
-		// display
-		panelApl.canv = new canvasManager.canv(ctx, canvas.width,
-											   canvas.height, panelApl);
-		// set event
-		var $btn = $('#btn1'); // start button
-		$btn.mousedown(panelApl.update);
-		$btn.text("update");
-
-		setTimeout(panelApl.update(), 1000);
-
-		// update by initial parameter
-
-	});
-})(jQuery);
+$(function() {
+	var apl = new panelApl();
+});
 
