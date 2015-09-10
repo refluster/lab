e4bc77b55db3e36f5d477925df6c39ec957c257c
refluster <refluster@gmail.com>
2015-09-10 16:09:32 +0900

    js/016 merge two js files

--- js/016/js/canvas.js
+++ js/016/js/canvas.js
@@ -8,21 +8,15 @@ for (var i = 0; i < 5; i++) {
 
 var canvasManager = function(ctx, w, h) {
 	this.ctx = ctx; // the context
-	this.area = {w:w, h:h};  // the area
-	this.cvpos = {x:0, y:0};  // position of the canvas on the browser
-
-	var $cvdiv = $('#canvas');
-	this.cvpos.x = $cvdiv.offset().left;
-	this.cvpos.y = $cvdiv.offset().top;
-
-	this.color = 'white'
-
+	this.canvasWidth = w;
+	this.canvasHeight = h;
+	this.color = 'white';
 	this.leaf = [];
 };
 
 canvasManager.prototype.blank = function() {
 	this.ctx.fillStyle = 'black';
-	this.ctx.fillRect(0, 0, this.area.w, this.area.h);
+	this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 };
 
 canvasManager.prototype.init = function() {
@@ -37,8 +31,8 @@ canvasManager.prototype.clearLeaf = function() {
 canvasManager.prototype.addLeaf = function(number, rmin, rmax) {
 	for (var i = 0; i < number; i++) {
 		var rand = Math.floor(Math.random()*1024*1024);
-		var x = rand%(this.area.w - 1);
-		var y = rand%this.area.h;
+		var x = rand%(this.canvasWidth- 1);
+		var y = rand%this.canvasHeight;
 		var r = rmin + rand%(rmax - rmin);
 
 		this.leaf.push({
@@ -63,3 +57,73 @@ canvasManager.prototype.draw = function() {
 		this.ctx.stroke();
 	}.bind(this));
 };
+
+var panelApl = function() {
+	var $canvas = $('#canvas');
+	if ( ! $canvas[0] || ! $canvas[0].getContext ) { return false; }
+	var ctx = $canvas[0].getContext("2d");
+	ctx.globalCompositeOperation = "source-over";
+
+	// display
+	this.canv = new canvasManager(ctx, $canvas.width(), $canvas.height());
+	// set event
+	var $btn = $('#btn1'); // start button
+	$btn.mousedown(this.update.bind(this));
+	$btn.text("update");
+
+	setTimeout(this.update.bind(this), 1000);
+};
+
+panelApl.prototype.update = function() {
+	var number = document.form1.input_number.value;
+	var rmin = document.form1.input_rmin.value;
+	var rmax = document.form1.input_rmax.value;
+	var seconds = document.form1.input_seconds.value;;
+	var fps = document.form1.input_fps.value;;
+
+	// check if inputs are number
+	if (isNaN(number) || isNaN(rmin) || isNaN(rmax) ||
+		isNaN(seconds) || isNaN(fps)) {
+		//this.showmsg("incorrect input");
+		return;
+	}
+
+	number = Number(number);
+	rmin = Number(rmin);
+	rmax = Number(rmax);
+	seconds = Number(seconds);
+	fps = Number(fps);
+
+	if (number < 0 || rmin < 0 || rmax < 0 || seconds < 0 || fps < 0) {
+		//this.showmsg("values must be positive");
+		return;
+	}
+
+	if (rmin > rmax) {
+		//this.showmsg("rmax must be larger than rmin");
+		return;
+	}
+
+	this.canv.init();
+
+	var curNumber = 0;
+	var incNumber = number/(seconds*fps);
+
+	var addAndDraw = function() {
+		this.canv.clearLeaf();
+		if (curNumber + incNumber > number) {
+			this.canv.addLeaf(number - curNumber, rmin, rmax);
+		} else {
+			setTimeout(addAndDraw, 1000/fps);
+			this.canv.addLeaf(incNumber, rmin, rmax);
+			curNumber += incNumber;
+		}
+		this.canv.draw();
+	}.bind(this);
+
+	var timer = setTimeout(addAndDraw, 50);
+};
+
+$(function() {
+	var apl = new panelApl();
+});
