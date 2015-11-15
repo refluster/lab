1d55441b7b76875e4c2a2c37c41c7459aa0ca572
refluster <refluster@gmail.com>
2015-11-15 13:45:08 +0900

    js/051 separate source file of fish class

--- js/051/js/app.js
+++ js/051/js/app.js
@@ -74,56 +74,6 @@ App.prototype.init = function() {
 	window.addEventListener('resize', this.resize.bind(this), false);
 	setTimeout(this.resize.bind(this), 1);
 
-//////////////////////////////
-var Fish = function() {
-	this.color = Math.random()*0xffffff;
-
-	var material = new THREE.MeshBasicMaterial({
-        color: this.color,
-        side: THREE.DoubleSide
-    });
-    var geometry = new THREE.Geometry();
-	geometry.vertices.push(new THREE.Vector3( 0,  0,  0));
-	geometry.vertices.push(new THREE.Vector3(10,  0, 10));
-	geometry.vertices.push(new THREE.Vector3(20,  0,  0));
-	geometry.vertices.push(new THREE.Vector3(25,  0,  5));
-	geometry.vertices.push(new THREE.Vector3(25,  0, -5));
-	geometry.vertices.push(new THREE.Vector3(20,  0,  0));
-	geometry.vertices.push(new THREE.Vector3(10,  0,-10));
-	geometry.vertices.push(new THREE.Vector3( 0,  0,  0));
-	geometry.faces.push(new THREE.Face3(0, 1, 2));
-	geometry.faces.push(new THREE.Face3(2, 3, 4));
-	geometry.faces.push(new THREE.Face3(5, 6, 7));
-
-	this.line = new THREE.Mesh(geometry, material);
-	this.state = 0;
-};
-
-Fish.prototype.setSeed = function(s) {
-	this.state = s;
-};
-
-Fish.prototype.setSize = function(s) {
-	this.line.position.set(x, y, z);
-};
-
-Fish.prototype.get3DObject = function() {
-	return this.line;
-};
-
-Fish.prototype.animate = function() {
-	this.line.geometry.vertices.forEach(function(v) {
-		v.y = Math.cos(v.x / 8 - this.state);
-	}.bind(this));
-	this.line.geometry.verticesNeedUpdate = true;
-	this.state += 0.1;
-};
-
-Fish.prototype.setPosition = function(x, y, z) {
-	this.line.position.set(x, y, z);
-};
-//////////////////////////////
-
 	this.fish = new Fish();
 	this.fish.setPosition(20, 40, 100);
 	this.fish.setSeed(0);
@@ -159,7 +109,6 @@ App.prototype.animate = function(t) {
 	this.update(this.clock.getDelta());
 	this.render(this.clock.getDelta());
 
-	//////////////////////////////
 	this.fish.animate();
 }
 
