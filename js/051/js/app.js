f067bc31ea8256a14499dde17b456a107222fdb4
refluster <refluster@gmail.com>
2015-11-15 10:18:43 +0900

    js/051 add a fish into the space

--- js/051/js/app.js
+++ js/051/js/app.js
@@ -7,6 +7,8 @@ var clock = new THREE.Clock();
 init();
 animate();
 
+var fish;
+
 function init() {
 	renderer = new THREE.WebGLRenderer();
 	element = renderer.domElement;
@@ -74,6 +76,61 @@ function init() {
 
 	window.addEventListener('resize', resize, false);
 	setTimeout(resize, 1);
+
+//////////////////////////////
+var Fish = function() {
+	this.color = Math.random()*0xffffff;
+
+	var material = new THREE.MeshBasicMaterial({
+        color: this.color,
+        side: THREE.DoubleSide
+    });
+    var geometry = new THREE.Geometry();
+	geometry.vertices.push(new THREE.Vector3( 0,  0,  0));
+	geometry.vertices.push(new THREE.Vector3(10,  0, 10));
+	geometry.vertices.push(new THREE.Vector3(20,  0,  0));
+	geometry.vertices.push(new THREE.Vector3(25,  0,  5));
+	geometry.vertices.push(new THREE.Vector3(25,  0, -5));
+	geometry.vertices.push(new THREE.Vector3(20,  0,  0));
+	geometry.vertices.push(new THREE.Vector3(10,  0,-10));
+	geometry.vertices.push(new THREE.Vector3( 0,  0,  0));
+	geometry.faces.push(new THREE.Face3(0, 1, 2));
+	geometry.faces.push(new THREE.Face3(2, 3, 4));
+	geometry.faces.push(new THREE.Face3(5, 6, 7));
+
+	this.line = new THREE.Mesh(geometry, material);
+	this.state = 0;
+};
+
+Fish.prototype.setSeed = function(s) {
+	this.state = s;
+};
+
+Fish.prototype.setSize = function(s) {
+	this.line.position.set(x, y, z);
+};
+
+Fish.prototype.get3DObject = function() {
+	return this.line;
+};
+
+Fish.prototype.animate = function() {
+	this.line.geometry.vertices.forEach(function(v) {
+		v.y = Math.cos(v.x / 8 - this.state);
+	}.bind(this));
+	this.line.geometry.verticesNeedUpdate = true;
+	this.state += 0.1;
+};
+
+Fish.prototype.setPosition = function(x, y, z) {
+	this.line.position.set(x, y, z);
+};
+//////////////////////////////
+
+	fish = new Fish();
+	fish.setPosition(20, 40, 100);
+	fish.setSeed(0);
+	scene.add(fish.get3DObject());
 }
 
 function resize() {
@@ -104,6 +161,9 @@ function animate(t) {
 
 	update(clock.getDelta());
 	render(clock.getDelta());
+
+	//////////////////////////////
+	fish.animate();
 }
 
 function fullscreen() {
@@ -117,3 +177,4 @@ function fullscreen() {
 		container.webkitRequestFullscreen();
 	}
 }
+
