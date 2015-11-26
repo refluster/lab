a4bdf6cc715b99c9000cbe70f79c8c040ef408d6
refluster <refluster@gmail.com>
2015-11-27 07:53:19 +0900

    js/052 refactor by removing comment

--- js/052/js/app.js
+++ js/052/js/app.js
@@ -1,11 +1,6 @@
 function load_file () {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
-/*
-    var kernel = [0.11,0.11,0.11,
-                  0.11,0.12,0.11,
-                  0.11,0.12,0.11];
-*/
     var kernel = [0.05,0.10,0.05,
                   0.08,0.13,0.08,
                   0.05,0.10,0.05];
@@ -17,18 +12,18 @@ function load_file () {
 		var image = new Image();
 		image.onload = function(){
 			var w = 320, h = 240;
-			var smooth = new Uint8ClampedArray(w*h*4);      // 出力画像格納用
+			var smooth = new Uint8ClampedArray(w*h*4);
 
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			
 			canvas.width = w;
 			canvas.height = h;
 			ctx.drawImage(image, 0, 0, w, h);
 
 			document.getElementById('text').innerHTML = "orig: " + image.width + " " + image.height;
 
-			var im = ctx.getImageData(0, 0, w, h);  // canvusから画像データを取得
-			//filter2d(im.data, smooth, w, h, kernel); // 画像にフィルタ処理
-			bin(im.data, smooth, w, h); // 画像にフィルタ処理
+			var im = ctx.getImageData(0, 0, w, h);
+			filter(im.data, smooth, w, h);
 			im.data.set(smooth);
 			ctx.putImageData(im, 0, 0);
 
@@ -78,20 +73,11 @@ function filter2d(src, dst, w, h, k) {
 	}
 }
 
-function bin(src, dst, w, h) {
+function filter(src, dst, w, h) {
 	var x, y, i;
 	var r = [];
 	var g = [];
 	var b = [];
-
-	for (i = 0; i < h; i++) {
-		for (j = 0; j < w; j++) {
-			var p = j + i * w;
-			r[p] = src[p*4 + 0];
-			g[p] = src[p*4 + 1];
-			b[p] = src[p*4 + 2];
-		}
-	}
 
 	var median = function(arr) {
 		var half = (arr.length/2)|0;
@@ -104,29 +90,34 @@ function bin(src, dst, w, h) {
 		return (temp[half-1] + temp[half])/2;
 	};
 
-	var med_r = median(r);
-	var med_g = median(g);
-	var med_b = median(b);
+	for (i = 0; i < h; i++) {
+		for (j = 0; j < w; j++) {
+			var p = j + i * w;
+			r[p] = src[p*4 + 0];
+			g[p] = src[p*4 + 1];
+			b[p] = src[p*4 + 2];
+		}
+	}
 
-	console.log(med_r);
-	console.log(med_g);
-	console.log(med_b);
-
-	var th_r = parseInt(med_r * .8);
-	var th_g = parseInt(med_g * .8);
-	var th_b = parseInt(med_b * .8);
+	var th_r = parseInt(median(r) * .8);
+	var th_g = parseInt(median(g) * .8);
+	var th_b = parseInt(median(b) * .8);
 
 	for (i = 0; i < h; i++) {
 		for (j = 0; j < w; j++) {
 			var p = j + i * w;
-			dst[p*4 + 0] = (src[p*4 + 0] > th_r ? 255: 0);
-			dst[p*4 + 1] = (src[p*4 + 1] > th_g ? 255: 0);
-			dst[p*4 + 2] = (src[p*4 + 2] > th_b ? 255: 0);
+			if (src[p*4 + 0] > th_r &&
+				src[p*4 + 1] > th_g &&
+				src[p*4 + 2] > th_b) {
+				dst[p*4 + 0] = 255;
+				dst[p*4 + 1] = 255;
+				dst[p*4 + 2] = 255;
+			} else {
+				dst[p*4 + 0] = 0;
+				dst[p*4 + 1] = 0;
+				dst[p*4 + 2] = 0;
+			}				
 			dst[p*4 + 3] = 255;
 		}
 	}
-
-	console.log(th_r);
-	console.log(th_g);
-	console.log(th_b);
 }
