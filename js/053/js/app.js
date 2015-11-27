1a536e011dc937be51b51a1a189677fd3970e556
refluster <refluster@gmail.com>
2015-11-27 20:58:52 +0900

    js/053 add list

--- js/053/js/app.js
+++ js/053/js/app.js
@@ -23,7 +23,50 @@ app.controller('SheetListController', [function SheetListController() {
 	
 }]);
 
-app.controller('CreationController', [function CreationController() {
+app.controller('CreationController', ['$scope', function CreationController($scope) {
+	function createOrderLine() {
+		return {
+			productName: '',
+			unitPrice: 0,
+			count: 0
+		};
+	}
+
+	$scope.lines = [createOrderLine()];
+
+	$scope.initialize = function () {
+		$scope.lines = [createOrderLine()];
+	};
+
+	$scope.addLine = function () {
+		$scope.lines.push(createOrderLine());
+	};
+
+	$scope.save = function () {
+	};
+
+	$scope.removeLine = function (target) {
+		var lines = $scope.lines;
+		var index = lines.indexOf(target);
+
+		if (index !==  -1) {
+			lines.splice(index, 1);
+		}
+	};
+
+	$scope.getSubtotal = function (orderLine) {
+		return orderLine.unitPrice * orderLine.count;
+	};
+
+	$scope.getTotalAmount = function (lines) {
+		var totalAmount = 0;
+
+		angular.forEach(lines, function (orderLine) {
+			totalAmount += $scope.getSubtotal(orderLine);
+		});
+
+		return totalAmount;
+	};
 }]);
 
 app.controller('SheetController', [function SheetController() {
