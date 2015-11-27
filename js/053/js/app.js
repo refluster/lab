2f3b54a69bcdd3a7801136e610460c617f719a67
refluster <refluster@gmail.com>
2015-11-27 20:33:36 +0900

    js/053 add controller

--- js/053/js/app.js
+++ js/053/js/app.js
@@ -3,15 +3,28 @@ var app = angular.module('App', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'index-tmpl'
+			templateUrl: 'index-tmpl',
+			controller: 'SheetListController'
 		})
 		.when('/new', {
-			templateUrl: 'new-tmpl'
+			templateUrl: 'new-tmpl',
+			controller: 'CreationController'
 		})
 		.when('/sheet/:id', {
-			templateUrl: 'sheet-tmpl'
+			templateUrl: 'sheet-tmpl',
+			controller: 'SheetController'
 		})
 		.otherwise({
 			redirectTo: '/'
 		});	
+}]);
+
+app.controller('SheetListController', [function SheetListController() {
+	
+}]);
+
+app.controller('CreationController', [function CreationController() {
+}]);
+
+app.controller('SheetController', [function SheetController() {
 }]);
