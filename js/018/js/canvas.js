114f8d6d2d7a6268bba7c455128e0090055e7f78
refluster <refluster@gmail.com>
2015-11-30 07:08:28 +0900

    js/018 disable stop/start button operation, disable mouse/touch event

--- js/018/js/canvas.js
+++ js/018/js/canvas.js
@@ -2,134 +2,134 @@
 var canvasManager = {}; // namespace
 
 (function($) {
-    /* canvas class
-     * {canvas context} ctx: the context
-     * {int} w: width
-     * {int} h: height
-     * {objext} names: caller namespace
-     */
+	/* canvas class
+	 * {canvas context} ctx: the context
+	 * {int} w: width
+	 * {int} h: height
+	 * {objext} names: caller namespace
+	 */
 
-    canvasManager.canv = function(ctx, w, h, names) {
+	canvasManager.canv = function(ctx, w, h, names) {
 		this.ctx = ctx; // the context
 		this.area = {w:w, h:h};  // the area
 		this.cvpos = {x:0, y:0};  // position of the canvas on the browser
-        this.prevPos = {x:0, y:0}; // previous position of the cursor
-        this.color = 'black'
-        this.lineWidth = 1;
-        this.PI2 = Math.PI * 2; // 2*pi
-        
+		this.prevPos = {x:0, y:0}; // previous position of the cursor
+		this.color = 'black'
+		this.lineWidth = 1;
+		this.PI2 = Math.PI * 2; // 2*pi
+
 		// set the position of the canvas on the browser
 		var $cvdiv = $('#cvdiv1');
 		this.cvpos.x = $cvdiv.offset().left;
 		this.cvpos.y = $cvdiv.offset().top;
 
-        this.gravity = 9.8; // m/s^2
-        this.drawInterval = 20; // msec, 1000/fps
-        this.theta = Math.PI/30;
-        this.px_per_meter = (300/0.06); // 300px per 6cm
+		this.gravity = 9.8; // m/s^2
+		this.drawInterval = 20; // msec, 1000/fps
+		this.theta = Math.PI/30;
+		this.px_per_meter = (300/0.06); // 300px per 6cm
 
-        this.center = {x:200, y:200};
-        this.accel = this.gravity*this.px_per_meter*Math.sin(this.theta);
+		this.center = {x:200, y:200};
+		this.accel = this.gravity*this.px_per_meter*Math.sin(this.theta);
 
-        this.radius = 14; // raduis of balls
-        this.ball = [];
+		this.radius = 14; // raduis of balls
+		this.ball = [];
 
 		this.blank = function() {
 			//this.ctx.clearRect(0, 0, this.area.w, this.area.h);
-            this.ctx.fillStyle = 'black';
-            this.ctx.fillRect(0, 0, this.area.w, this.area.h);
-		};
-        
-        this.init = function() {
-            this.ball[0] = {
-                pos:{x:100, y:100},
-                v:{x:2, y:4},
-            };
-            this.ball[1] = {
-                pos:{x:200, y:100},
-                v:{x:3, y:4},
-            };
-            this.ball[2] = {
-                pos:{x:150, y:110},
-                v:{x:2, y:-4},
-            };
-            this.ball[3] = {
-                pos:{x:280, y:210},
-                v:{x:2, y:-4},
-            };
-            this.ball[4] = {
-                pos:{x:500, y:180},
-                v:{x:2, y:-4},
-            };
-        };
-        
-        this.getNormalVector = function(dst, src) {
-            var v = {x:dst.x - src.x, y:dst.y - src.y};
-            var d = Math.sqrt(v.x*v.x + v.y*v.y);
-            return {x:v.x/d, y:v.y/d};
-        };
-        
-        this.setFps = function(fps) {
-            this.drawInterval = 1000/fps;
-        };
-
-        this.setGravity = function(gravity) {
-            this.gravity = gravity;
-            this.accel = this.gravity*this.px_per_meter*Math.sin(this.theta);
-        };
-        
-        this.moveObj = function() {
-            // (m/s/s)*(interval)*(interval)
-            var intervalAccel = this.accel*
-                this.drawInterval*this.drawInterval/1000000;
-
-            // calc pos
-            for (var i = 0; i < this.ball.length; i++) {
-                this.ball[i].pos.x += this.ball[i].v.x;
-                this.ball[i].pos.y += this.ball[i].v.y;
-            }
-
-            // calc vel (inc accel)
-            for (var i = 0; i < this.ball.length; i++) {
-                var p = this.ball[i].pos;
-                var nvec = this.getNormalVector(this.center, this.ball[i].pos);
-                this.ball[i].v.x += nvec.x*intervalAccel;
-                this.ball[i].v.y += nvec.y*intervalAccel;
-            }
-            
-        };
-
-        this.draw = function() {
-            this.blank();
-            this.ctx.save();
-
-            this.ctx.strokeStyle = 'rgb(160,160,160)';
-            var r = 600;
-            var n = 13;
-            this.ctx.beginPath();
-            for (var i = 0; i < n; i++) {
-                var dx = r*Math.cos(Math.PI/n*i);
-                var dy = r*Math.sin(Math.PI/n*i);
-                this.ctx.moveTo(this.center.x + dx, this.center.y + dy);
-                this.ctx.lineTo(this.center.x - dx, this.center.y - dy);
-            }
-            this.ctx.stroke();
-
-            // draw ball
-            this.ctx.fillStyle = 'white';
-            //this.ctx.globalAlpha = 0.5;
-            for (var i = 0; i < this.ball.length; i++) {
-                this.ctx.fillStyle = this.ball[i].color;
-                this.ctx.beginPath();
-                this.ctx.arc(this.ball[i].pos.x, this.ball[i].pos.y,
-                             this.radius, 0, this.PI2, false);
-                this.ctx.fill();
-            }
-
-            this.ctx.restore();
+			this.ctx.fillStyle = 'black';
+			this.ctx.fillRect(0, 0, this.area.w, this.area.h);
 		};
 
-    }
+		this.init = function() {
+			this.ball[0] = {
+				pos:{x:100, y:100},
+				v:{x:2, y:4},
+			};
+			this.ball[1] = {
+				pos:{x:200, y:100},
+				v:{x:3, y:4},
+			};
+			this.ball[2] = {
+				pos:{x:150, y:110},
+				v:{x:2, y:-4},
+			};
+			this.ball[3] = {
+				pos:{x:280, y:210},
+				v:{x:2, y:-4},
+			};
+			this.ball[4] = {
+				pos:{x:500, y:180},
+				v:{x:2, y:-4},
+			};
+		};
+
+		this.getNormalVector = function(dst, src) {
+			var v = {x:dst.x - src.x, y:dst.y - src.y};
+			var d = Math.sqrt(v.x*v.x + v.y*v.y);
+			return {x:v.x/d, y:v.y/d};
+		};
+
+		this.setFps = function(fps) {
+			this.drawInterval = 1000/fps;
+		};
+
+		this.setGravity = function(gravity) {
+			this.gravity = gravity;
+			this.accel = this.gravity*this.px_per_meter*Math.sin(this.theta);
+		};
+
+		this.moveObj = function() {
+			// (m/s/s)*(interval)*(interval)
+			var intervalAccel = this.accel*
+				this.drawInterval*this.drawInterval/1000000;
+
+			// calc pos
+			for (var i = 0; i < this.ball.length; i++) {
+				this.ball[i].pos.x += this.ball[i].v.x;
+				this.ball[i].pos.y += this.ball[i].v.y;
+			}
+
+			// calc vel (inc accel)
+			for (var i = 0; i < this.ball.length; i++) {
+				var p = this.ball[i].pos;
+				var nvec = this.getNormalVector(this.center, this.ball[i].pos);
+				this.ball[i].v.x += nvec.x*intervalAccel;
+				this.ball[i].v.y += nvec.y*intervalAccel;
+			}
+
+		};
+
+		this.draw = function() {
+			this.blank();
+			this.ctx.save();
+
+			this.ctx.strokeStyle = 'rgb(160,160,160)';
+			var r = 600;
+			var n = 13;
+			this.ctx.beginPath();
+			for (var i = 0; i < n; i++) {
+				var dx = r*Math.cos(Math.PI/n*i);
+				var dy = r*Math.sin(Math.PI/n*i);
+				this.ctx.moveTo(this.center.x + dx, this.center.y + dy);
+				this.ctx.lineTo(this.center.x - dx, this.center.y - dy);
+			}
+			this.ctx.stroke();
+
+			// draw ball
+			this.ctx.fillStyle = 'white';
+			//this.ctx.globalAlpha = 0.5;
+			for (var i = 0; i < this.ball.length; i++) {
+				this.ctx.fillStyle = this.ball[i].color;
+				this.ctx.beginPath();
+				this.ctx.arc(this.ball[i].pos.x, this.ball[i].pos.y,
+							 this.radius, 0, this.PI2, false);
+				this.ctx.fill();
+			}
+
+			this.ctx.restore();
+		};
+
+	}
 
 })(jQuery);
 
--- js/018/js/panel.js
+++ js/018/js/panel.js
@@ -5,117 +5,55 @@ var panelApl = {}; // namespace
 
 (function($) {
 
-    /* global var */
-    // drag state
-    panelApl.drag = {
+	/* global var */
+	// drag state
+	panelApl.drag = {
 		now: false, // true if dragging
-    };
-    panelApl.gamestart = false;  // true if playing
+	};
+	panelApl.gamestart = false;  // true if playing
 
-    // timer
-    panelApl.timer = $.timer();
+	// timer
+	panelApl.timer = $.timer();
 
-    /* button process
-     * return: none
-     */
-    panelApl.start = function() {
+	/* button process
+	 * return: none
+	 */
+	panelApl.start = function() {
 		var $cvdiv = $('#cvdiv1'); // main Canvas¤Îdiv
-		var $btn = $('#stbtn1'); // start button
-		if (!panelApl.gamestart) { // if not playing
-            //// set parameters from web form
-            var gravity = document.form1.input_gravity.value;
-            var fps = document.form1.input_fps.value;
-            // check if inputs are number
-            if (isNaN(gravity) || isNaN(fps)) {
-                return;
-            }
-            gravity = Number(gravity);
-            fps = Number(fps);
-            if (gravity < 0 || fps < 0) {
-                return;
-            }
-            panelApl.canv.setGravity(gravity);
-            panelApl.canv.setFps(fps);
 
-			// add mouse events to the canvas
-			$cvdiv.mousedown(panelApl.cvmsDown);
-			$cvdiv.mouseup(panelApl.cvmsUp);
-			$cvdiv.mouseleave(panelApl.cvmsUp);
-			$cvdiv.mousemove(panelApl.cvmsMove);
-			// add touch events to the canvas
-			$cvdiv.bind("touchstart", panelApl.cvmsDown);
-			$cvdiv.bind("touchend", panelApl.cvmsUp);
-			$cvdiv.bind("touchend", panelApl.cvmsUp);
-			$cvdiv.bind("touchmove", panelApl.cvmsMove);
-
-			// init canvas
-			panelApl.canv.init();
-
-			panelApl.gamestart = true;
-
-
-            panelApl.timer.set({
-                action: function() {
-                    panelApl.canv.moveObj();
-                    panelApl.canv.draw();
-                },
-                time: 1000/fps
-            });
-
-            panelApl.timer.play();
-			$btn.text('stop');
-		} else { // if playing
-			// delete mouse events from the canvas
-			$cvdiv.unbind('mousedown', panelApl.cvmsDown);
-			$cvdiv.unbind('mouseup', panelApl.cvmsUp);
-			$cvdiv.unbind('mouseleave', panelApl.cvmsUp);
-			$cvdiv.unbind('mousemove', panelApl.cvmsMove);
-			// delete touch events from the canvas
-			$cvdiv.unbind("touchstart", panelApl.cvmsDown);
-			$cvdiv.unbind("touchend", panelApl.cvmsUp);
-			$cvdiv.unbind("touchend", panelApl.cvmsUp);
-			$cvdiv.unbind("touchmove", panelApl.cvmsMove);
-
-			panelApl.gamestart = false;
-            panelApl.timer.pause();
-			$btn.text('start');
+		// set parameters from web form
+		var gravity = document.form1.input_gravity.value;
+		var fps = document.form1.input_fps.value;
+		// check if inputs are number
+		if (isNaN(gravity) || isNaN(fps)) {
+			return;
 		}
-    };
-
-    panelApl.cvmsDown = function(evt) {
-		// convert coordinate from point to canvas
-		var cx = evt.pageX - panelApl.canv.cvpos.x;
-		var cy = evt.pageY - panelApl.canv.cvpos.y;
-		panelApl.drag.now = true;
-		return false;
-    };
-
-    panelApl.cvmsUp = function(evt) {
-		if (panelApl.drag.now) {
-			// convert coordinate from point to canvas
-			var cx = evt.pageX - panelApl.canv.cvpos.x;
-			var cy = evt.pageY - panelApl.canv.cvpos.y;
-			if (cx < 0) cx = 0;
-			if (cx > panelApl.canv.area.w) cx = panelApl.canv.area.w;
-			if (cy < 0) cy = 0;
-			if (cy > panelApl.canv.area.h) cy = panelApl.canv.area.h;
-
-			panelApl.drag.now = false;
+		gravity = Number(gravity);
+		fps = Number(fps);
+		if (gravity < 0 || fps < 0) {
+			return;
 		}
-    };
+		panelApl.canv.setGravity(gravity);
+		panelApl.canv.setFps(fps);
 
-    panelApl.cvmsMove = function(evt) {
-		if (panelApl.drag.now) {
-			// convert coordinate from point to canvas
-			var cx = evt.pageX - panelApl.canv.cvpos.x;
-			var cy = evt.pageY - panelApl.canv.cvpos.y;
-            //panelApl.canv.draw({x:cx, y:cy});
-		}
-		return false;
-    };
+		// init canvas
+		panelApl.canv.init();
 
-    /* body onload process */
-    $(window).load(function() {
+		panelApl.gamestart = true;
+
+		panelApl.timer.set({
+			action: function() {
+				panelApl.canv.moveObj();
+				panelApl.canv.draw();
+			},
+			time: 1000/fps
+		});
+
+		panelApl.timer.play();
+	};
+
+	/* body onload process */
+	$(window).load(function() {
 		// get canvas's DOM element and context
 		var canvas = document.getElementById('cv1');
 		if ( ! canvas || ! canvas.getContext ) { return false; }
@@ -125,13 +63,13 @@ var panelApl = {}; // namespace
 
 		// display
 		panelApl.canv = new canvasManager.canv(ctx, canvas.width,
-                                               canvas.height, panelApl);
+											   canvas.height, panelApl);
 		panelApl.canv.init();
 		panelApl.canv.draw();
-        
+
 		// set events
 		var $btn = $('#stbtn1'); // start button
 		$btn.mousedown(panelApl.start);
-		$btn.text('start');
-    });
+		$btn.text('update');
+	});
 })(jQuery);
