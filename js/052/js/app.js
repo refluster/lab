e10febc769b6cf1cc19737a35c1d0c6c8dbca7a0
refluster <refluster@gmail.com>
2015-11-30 23:42:53 +0900

    js/052 enable to specify a rectangle area on the canvas

--- js/052/js/app.js
+++ js/052/js/app.js
@@ -4,6 +4,18 @@ var App = function() {
 
 	this.canvas.width = this.canvas.height = 0;
 	document.getElementById('picker').onchange = this.loadFile.bind(this);
+
+	this.fgImage = document.getElementById("fgImage");
+	this.fgContext = fgImage.getContext("2d");
+
+	this.fgImage.style.opacity = 0.4;
+	this.fgContext.fillStyle = "rgb(180,0,0)";
+
+	this.fgImage.addEventListener("touchstart", this.touchStartHandler.bind(this), false);
+	this.fgImage.addEventListener("touchmove", this.touchMoveHandler.bind(this), false);
+	this.fgImage.addEventListener("touchend", this.touchEndHandler.bind(this), false);
+
+	this.isDragging = false;
 };
 
 App.prototype.loadFile = function() {
@@ -39,10 +51,47 @@ App.prototype.loadFile = function() {
 					document.getElementById('transcription').innerText = result.text;
 				});
 			};
-		}.bind(this)
+		}.bind(this);
 		image.src = reader.result;
-	}.bind(this)
-	reader.readAsDataURL(document.getElementById('picker').files[0])
+	}.bind(this);
+	reader.readAsDataURL(document.getElementById('picker').files[0]);
+};
+
+App.prototype.getCanvasPosition = function(e) {
+	if (e.touches.length > 0) {
+		var b = this.fgImage.getBoundingClientRect();
+		return {x: parseInt(e.touches[0].pageX - b.left),
+				y: parseInt(e.touches[0].pageY - b.top)};
+	} else {
+		return {x: parseInt(e.pageX),
+				y: parseInt(e.pageY)};
+	}
+};
+
+App.prototype.touchStartHandler = function(e) {
+	this.dragStart = this.getCanvasPosition(e);
+	this.isDragging = true;
+	e.preventDefault();
+};
+
+App.prototype.touchEndHandler = function(e) {
+	if (this.isDragging) {
+		this.isDragging = false;
+	}
+	e.preventDefault();
+};
+
+App.prototype.touchMoveHandler = function(e) {
+	if (this.isDragging) {
+		var p = this.getCanvasPosition(e);
+		var w = p.x - this.dragStart.x;
+		var h = p.y - this.dragStart.y;
+		this.fgContext.clearRect(0, 0, this.fgImage.width, this.fgImage.height);
+		this.fgContext.fillRect(this.dragStart.x, this.dragStart.y, w, h);
+		//this.fgContext.fillRect(10, 10, 100, -150);
+		console.log({l: this.dragStart.x, u: this.dragStart.y, w: w, h: h});
+	}
+	e.preventDefault();
 };
 
 window.onload = function() {
