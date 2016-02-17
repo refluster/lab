f7cbe1bbe1171c2582d62a463e282857b1385b6f
refluster <refluster@gmail.com>
2016-02-17 23:12:31 +0900

    js/054 bugfix

--- js/054/js/app.js
+++ js/054/js/app.js
@@ -7,11 +7,13 @@ var App = function() {
 
 App.prototype.init = function() {
 	this.renderer = new THREE.WebGLRenderer();
-	this.renderer.setClearColor(new THREE.Color(0x000000));
+	this.renderer.setClearColor(new THREE.Color(0x60a0f0));
 	this.element = this.renderer.domElement;
 	this.container = document.getElementById('example');
 	this.container.appendChild(this.element);
 	this.clock = new THREE.Clock();
+
+	this.renderer.shadowMapEnabled = true;
 
 	this.effect = new THREE.StereoEffect(this.renderer);
 	this.effect.separation = .1;
@@ -19,8 +21,10 @@ App.prototype.init = function() {
 	this.scene = new THREE.Scene();
 
 	this.camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-	this.camera.position.set(20, 80, 0);
-	this.camera.position.set(0, 10, 0);
+	//	this.camera.position.set(80, 1, .1);
+	this.camera.position.x = 80;
+	this.camera.position.y = 1.8;
+	this.camera.position.x = 10;
 	this.scene.add(this.camera);
 
 	this.controls = new THREE.OrbitControls(this.camera, this.element);
@@ -45,16 +49,16 @@ App.prototype.init = function() {
 	window.addEventListener('deviceorientation', setOrientationControls, true);
 
 
-	var hemiLight = new THREE.HemisphereLight(0xbbaaaa, 0x000000, 0.1);
+	var hemiLight = new THREE.HemisphereLight(0xbbaaaa, 0x000000, 0.3);
 	this.scene.add(hemiLight);
 
-	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
 	directionalLight.position.set( -1, 1, 0 );
+	directionalLight.castShadow = true;
 	this.scene.add( directionalLight );
 
-	var ambLight = new THREE.AmbientLight( 0x202020 ); // soft white light
+	var ambLight = new THREE.AmbientLight( 0x303030 ); // soft white light
 	this.scene.add( ambLight );
-
 
 	var texture = THREE.ImageUtils.loadTexture(
 		'textures/patterns/asp1.jpg'
@@ -76,6 +80,7 @@ App.prototype.init = function() {
 
 	var mesh = new THREE.Mesh(geometry, material);
 	mesh.rotation.x = -Math.PI / 2;
+	mesh.receiveShadow = true;
 	this.scene.add(mesh);
 
 	window.addEventListener('resize', this.resize.bind(this), false);
@@ -101,29 +106,81 @@ App.prototype.initObject = function(){
 		'js/build10.json',
 		'js/build11.json',
 		'js/build12.json',
+		'js/signal.json',
 	];
 
 	var map = [
 		{objId: 10, x:   0, y:   0},
-		{objId:  1, x:   0, y:  40},
-		{objId:  2, x:   0, y:  80},
-		{objId: 11, x:   0, y: 160},
-		{objId: 10, x:   0, y: 200},
-		{objId:  5, x:   0, y: 240},
-		{objId:  2, x:  40, y: 280},
-		{objId:  0, x:  40, y: 320},
-		{objId: 10, x:  40, y: 360},
+		{objId:  7, x:   0, y:  30},
+		{objId:  2, x:   0, y:  90},
+		{objId: 11, x:   0, y: 120},
+		{objId: 10, x:   0, y: 150},
+		{objId:  5, x:   0, y: 180},
+		{objId:  8, x:   0, y: 210},
+		{objId:  9, x:   0, y: 240},
+		{objId: 10, x:   0, y: 270},
+		{objId: 12, x:   0, y: 300},
 
-		{objId:  2, x:  40, y:   0},
-		{objId:  0, x:  40, y:  40},
-		{objId:  1, x:  40, y:  80},
-		{objId: 11, x:  40, y: 120},
-		{objId: 10, x:  40, y: 160},
-		{objId:  5, x:  40, y: 200},
-		{objId:  6, x:  40, y: 240},
-		{objId:  6, x:  40, y: 280},
-		{objId: 10, x:  40, y: 320},
-		{objId:  5, x:  40, y: 360},
+		{objId:  8, x:  20, y:   0},
+		{objId:  0, x:  20, y:  30},
+		{objId:  9, x:  20, y:  90},
+		{objId: 11, x:  20, y: 120},
+		{objId: 10, x:  20, y: 150},
+		{objId:  5, x:  20, y: 180},
+		{objId:  6, x:  20, y: 210},
+		{objId:  6, x:  20, y: 240},
+		{objId: 10, x:  20, y: 270},
+		{objId:  5, x:  20, y: 300},
+
+		{objId: 10, x:  40, y:   0},
+		{objId: 12, x:  40, y:  30},
+		{objId:  5, x:  40, y:  90},
+		{objId: 10, x:  40, y: 120},
+		{objId:  8, x:  40, y: 150},
+		{objId:  7, x:  40, y: 180},
+		{objId:  5, x:  40, y: 210},
+		{objId: 10, x:  40, y: 240},
+		{objId: 12, x:  40, y: 270},
+		{objId: 12, x:  40, y: 300},
+
+		{objId: 13, x:  60, y:  10},
+		{objId: 13, x:  60, y:  40},
+		{objId: 13, x:  60, y:  80},
+		{objId: 13, x:  60, y: 100},
+
+		{objId: 12, x:  80, y:   0},
+		{objId: 10, x:  80, y:  30},
+		{objId:  5, x:  80, y:  90},
+		{objId: 10, x:  80, y: 120},
+		{objId:  8, x:  80, y: 150},
+		{objId:  7, x:  80, y: 180},
+		{objId:  5, x:  80, y: 210},
+		{objId: 10, x:  80, y: 240},
+		{objId: 12, x:  80, y: 270},
+		{objId: 12, x:  80, y: 300},
+
+		{objId: 10, x: 100, y:   0},
+		{objId:  7, x: 100, y:  30},
+		{objId:  2, x: 100, y:  90},
+		{objId: 11, x: 100, y: 120},
+		{objId: 10, x: 100, y: 150},
+		{objId:  5, x: 100, y: 180},
+		{objId:  2, x: 100, y: 210},
+		{objId:  8, x: 100, y: 240},
+		{objId: 10, x: 100, y: 270},
+		{objId: 12, x: 100, y: 300},
+
+		{objId: 12, x: 120, y:   0},
+		{objId: 10, x: 120, y:  30},
+		{objId:  1, x: 120, y:  90},
+		{objId: 11, x: 120, y: 120},
+		{objId: 10, x: 120, y: 150},
+		{objId:  5, x: 120, y: 180},
+		{objId:  6, x: 120, y: 210},
+		{objId:  6, x: 120, y: 240},
+		{objId: 10, x: 120, y: 270},
+		{objId: 12, x: 120, y: 300},
+
 	];
 
 	var objmap = {};
@@ -168,6 +225,7 @@ App.prototype.initObject = function(){
 									  new THREE.MeshLambertMaterial({color: 0xffffff}));
 				mesh.position.x = o.x;
 				mesh.position.z = o.y;
+				mesh.castShadow = true;
 				this.scene.add(mesh);
 			}.bind(this));
 		}
@@ -185,9 +243,6 @@ App.prototype.resize = function() {
 	this.effect.setSize(width, height);
 };
 
-var v = 0;
-var t_ = 0;
-
 App.prototype.update = function(dt) {
 	this.resize();
 
@@ -196,14 +251,24 @@ App.prototype.update = function(dt) {
 	this.controls.update(dt);
 };
 
+var v = 0;
+
 App.prototype.render = function(dt) {
 	this.effect.render(this.scene, this.camera);
 
 	var d = this.camera.getWorldDirection();
 
 	this.camera.position.x += d.x/16;
-	this.camera.position.y += d.y/16;
+	//this.camera.position.y += d.y/16;
+	this.camera.position.y += v;
+	//this.camera.position.y = 1.8;
 	this.camera.position.z += d.z/16;
+
+	v -= 9.8*dt/40;
+	if (this.camera.position.y < 0) {
+		v = 0;
+		this.camera.position.y = 80;
+	}
 
 	this.controls.target.set(
 		this.camera.position.x + d.x/16,
@@ -215,8 +280,9 @@ App.prototype.render = function(dt) {
 App.prototype.animate = function(t) {
 	requestAnimationFrame(this.animate.bind(this));
 
-	this.update(this.clock.getDelta());
-	this.render(this.clock.getDelta());
+	var dt = this.clock.getDelta();
+	this.update(dt);
+	this.render(dt);
 };
 
 App.prototype.fullscreen = function() {
