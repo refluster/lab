e9e79e104bbadb44920444b6154b167d11ad7308
refluster <refluster@gmail.com>
2016-02-27 15:55:54 +0900

    js/020 tabify

--- js/020/js/panel.js
+++ js/020/js/panel.js
@@ -1,25 +1,25 @@
 (function($) {
-    /* body onload process */
-    $(window).load(function() {
-        window.addEventListener('devicemotion', function(evt) {
-            var ac = evt.acceleration;
-            var temp = "ac_x:" + ac.x +"<br>";
-            temp += "ac_y:" + ac.y +"<br>";
-            temp += "ac_z:" + ac.z +"<br>";
-            temp += "<br>";
+	/* body onload process */
+	$(window).load(function() {
+		window.addEventListener('devicemotion', function(evt) {
+			var ac = evt.acceleration;
+			var temp = "ac_x:" + ac.x +"<br>";
+			temp += "ac_y:" + ac.y +"<br>";
+			temp += "ac_z:" + ac.z +"<br>";
+			temp += "<br>";
 
-            var acg = evt.accelerationIncludingGravity;
-            temp += "acg_x:" + acg.x +"<br>";
-            temp += "acg_y:" + acg.y +"<br>";
-            temp += "acg_z:" + acg.z +"<br>";
-            temp += "<br>";
+			var acg = evt.accelerationIncludingGravity;
+			temp += "acg_x:" + acg.x +"<br>";
+			temp += "acg_y:" + acg.y +"<br>";
+			temp += "acg_z:" + acg.z +"<br>";
+			temp += "<br>";
 
-            var rr = evt.rotationRate;
-            temp += "rr_a:" + rr.alpha +"<br>";
-            temp += "rr_b:" + rr.beta +"<br>";
-            temp += "rr_g:" + rr.gamma +"<br>";
+			var rr = evt.rotationRate;
+			temp += "rr_a:" + rr.alpha +"<br>";
+			temp += "rr_b:" + rr.beta +"<br>";
+			temp += "rr_g:" + rr.gamma +"<br>";
 
-            document.getElementById("msg").innerHTML = temp;
-        });
-    });
+			document.getElementById("msg").innerHTML = temp;
+		});
+	});
 })(jQuery);
