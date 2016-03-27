d3095eb40ff8692e3a76978996ab2547feacfecf
refluster <refluster@gmail.com>
2016-03-27 16:58:26 +0900

    js/056 simplify

--- js/056/js/renderer.js
+++ js/056/js/renderer.js
@@ -269,15 +269,7 @@ gl_FragColor = vec4(0.2, 0.2, 0.0, 0.0);\
 \
 vec3 refractedLight = refract(-light, vec3(0.0, 1.0, 0.0), IOR_AIR / IOR_WATER);\
 \
-/* compute a blob shadow and make sure we only draw a shadow if the player is blocking the light */\
-vec3 dir = (sphereCenter - newPos) / sphereRadius;\
-vec3 area = cross(dir, refractedLight);\
-float shadow = dot(area, area);\
-float dist = dot(dir, -refractedLight);\
-shadow = 1.0 + (shadow - 1.0) / (0.05 + dist * 0.025);\
-shadow = clamp(1.0 / (1.0 + exp(-shadow)), 0.0, 1.0);\
-shadow = mix(1.0, shadow, clamp(dist * 2.0, 0.0, 1.0));\
-gl_FragColor.g = shadow;\
+gl_FragColor.g = 1.0;\
 \
 /* shadow for the rim of the pool */\
 vec2 t = intersectCube(newPos, -refractedLight, vec3(-1.0, -poolHeight, -1.0), vec3(1.0, 2.0, 1.0));\
@@ -295,8 +287,6 @@ Renderer.prototype.updateCaustics = function(water) {
 		this_.causticsShader.uniforms({
 			light: this_.lightDir,
 			water: 0,
-			sphereCenter: this_.sphereCenter,
-			sphereRadius: this_.sphereRadius
 		}).draw(this_.waterMesh);
 	});
 };
