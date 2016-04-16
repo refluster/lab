bc3e346636b4337388ac5539b1e381562b887742
refluster <refluster@gmail.com>
2016-04-17 01:39:54 +0900

    js/058 add d3 lib

--- js/058/js/main.js
+++ js/058/js/main.js
@@ -1,12 +1,37 @@
-var Apl = function() {
-	this.db = [];
+var Graph = function() {
 	this.svg = d3.select("#contents")
 		.append("svg")
 		.attr("width", 500)
 		.attr("height", 300);
+	this.dataReset();
+};
+
+Graph.prototype.dataReset = function() {
+	this.data = [];
+};
+
+Graph.prototype.dataSet = function(lng, lat, level) {
+	this.data.push(lng, lat, level);
+};
+
+Graph.prototype.update = function(dataset) {
+	this.svg.selectAll("circle")
+		.data(dataset)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d) { return d[0]; })
+		.attr("cy", function(d) { return d[1]; })
+		.attr("r",  function(d) { return d[2]; })
+		.fill("fill", "green");
+};
+
+var Apl = function() {
+	this.db = [];
 };
 
 Apl.prototype.getData = function() {
+	this.graph = new Graph();
+
 	$.get('./data.dat', function(data) {
 		var d = data.split('\r\n');
 		jQuery.each(d, function(i, val) {
@@ -51,18 +76,7 @@ Apl.prototype.showDB = function() {
 	$('#data').append(html);
 };
 
-Apl.prototype.graph = function(dataset) {
-	this.svg.selectAll("circle")
-		.data(dataset)
-		.enter()
-		.append("circle")
-		.attr("cx", function(d) { return d[0]; })
-		.attr("cy", function(d) { return d[1]; })
-		.attr("r",  function(d) { return d[2]; })
-		.fill("fill", "green");
-};
-
-$(function() {
+Apl.prototype.showGraph = function() {
 	var dataset = [
         [   5,   20 ,  3],
         [ 480,   90 ,  4],
@@ -76,7 +90,11 @@ $(function() {
         [ 220,   88 ,  6]
     ];
 
+	this.graph.update(dataset);
+};
+
+$(function() {
 	var apl = new Apl();
 	apl.getData();
-	apl.graph(dataset);
+	apl.showGraph();
 });
