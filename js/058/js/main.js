5ab7652e087ee53bcc16d08b08e5233dd5e9c21a
refluster <refluster@gmail.com>
2016-04-29 17:26:17 +0900

    js/058 set upper limit date

--- js/058/js/main.js
+++ js/058/js/main.js
@@ -20,12 +20,10 @@ var Graph = function() {
 };
 
 Graph.prototype.dataReset = function() {
-	var today = new Date().getDate();
-
 	this.data = [];
 	for (var year = 2016; year < 2017; year ++) {
 		for (var month = 4; month < 5; month ++) {
-			for (var day = 14; day < today + 1; day ++) {
+			for (var day = 14; day < 30; day ++) {
 				for (var hour = 0; hour < 24; hour ++) {
 					this.data.push({
 						date: new Date(year, month, day, hour),
