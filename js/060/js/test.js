dc7cf9bf8dd7e8f4a532081e6926f639c635050c
refluster <refluster@gmail.com>
2016-07-14 23:59:12 +0900

    sample site

--- js/060/js/test.js
+++ js/060/js/test.js
@@ -1,12 +1,8 @@
-const space = {x: [-200, 200], y: [5, 40], z: [-200, 200]};
-const fishNum = 60;
-
 var App = function() {
 	this.camera, this.scene, this.renderer;
 	this.effect, this.controls;
 	this.element, this.container;
 	this.clock;
-	this.fish;
 };
 
 App.prototype.init = function() {
@@ -22,51 +18,18 @@ App.prototype.init = function() {
 	this.scene = new THREE.Scene();
 
 	this.camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-	this.camera.position.set(0, 10, 0);
-//	this.camera.position.set(0, 200, 0);
+	this.camera.position.set(0, 0, 200);
 	this.scene.add(this.camera);
 
 	this.controls = new THREE.OrbitControls(this.camera, this.element);
 	this.controls.rotateUp(Math.PI / 4);
-	this.controls.target.set(
-		100, 200, 0
-//		100, 0, 0
-//		this.camera.position.x + 0.1,
-//		this.camera.position.y,
-//		this.camera.position.z
-	);
+	this.controls.target.set(0, 0, 0);
 	this.controls.noZoom = true;
 	this.controls.noPan = true;
 
-	var setOrientationControls = function(e) {
-		if (!e.alpha) {
-			return;
-		}
-
-		this.controls = new THREE.DeviceOrientationControls(this.camera, true);
-		this.controls.connect();
-		this.controls.update();
-
-		this.element.addEventListener('click', this.fullscreen.bind(this), false);
-
-		window.removeEventListener('deviceorientation', setOrientationControls, true);
-	}.bind(this)
-//	window.addEventListener('deviceorientation', setOrientationControls, true);
-
-
-/*
-	hemiLight = new THREE.HemisphereLight( 0xffffff,
-										   0xffffff, 0.6 );
-//	hemiLight.color.setHSL( 0.6, 1, 0.6 );
-//	hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-	hemiLight.position.set( 0, 500, 0 );
-	hemiLight.visible = true;
-	this.scene.add( hemiLight );
-*/
-
 	var light = new THREE.PointLight(0xffffff, 1.0);
 	// We want it to be very close to our character
-	light.position.set(100, 100, 100);
+	light.position.set(1000, 1000, 1000);
 	this.scene.add(light);
 
 	//////////////////////////////
@@ -77,9 +40,6 @@ App.prototype.init = function() {
 			fragmentShader: document.getElementById('fshader002').textContent,
 			uniforms: THREE.UniformsUtils.merge([
 				THREE.UniformsLib['lights'],
-				{
-					color: {type: 'f', value: 0.0},
-				}
 			]),
 			lights: true,
 		});
@@ -99,9 +59,6 @@ App.prototype.init = function() {
 			fragmentShader: document.getElementById('fshader001').textContent,
 			uniforms: THREE.UniformsUtils.merge([
 				THREE.UniformsLib['lights'],
-				{
-					color: {type: 'f', value: 0.0},
-				}
 			]),
 			lights: true,
 		});
@@ -112,54 +69,32 @@ App.prototype.init = function() {
 	}
 	//////////////////////////////
 
-	var texture = THREE.ImageUtils.loadTexture(
-		'textures/patterns/checker.png'
-	);
-	texture.wrapS = THREE.RepeatWrapping;
-	texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat = new THREE.Vector2(50, 50);
-	texture.anisotropy = this.renderer.getMaxAnisotropy();
+	////////////////////////////// floor
+	{
+		var texture = THREE.ImageUtils.loadTexture(
+            'textures/patterns/checker.png'
+		);
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		texture.repeat = new THREE.Vector2(50, 50);
+		texture.anisotropy = this.renderer.getMaxAnisotropy();
 
-	var material = new THREE.MeshPhongMaterial({
-		color: 0xffffff,
-		specular: 0xffffff,
-		shininess: 20,
-		shading: THREE.FlatShading,
-		map: texture,
-	});
-
-	var geometry = new THREE.PlaneGeometry(1000, 1000);
-
-	var mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.x = -Math.PI / 2;
-	this.scene.add(mesh);
+		var material = new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            specular: 0xffffff,
+            shininess: 20,
+            shading: THREE.FlatShading,
+            map: texture,
+		});
+		var geometry = new THREE.PlaneGeometry(1000, 1000);
+		var mesh = new THREE.Mesh(geometry, material);
+		mesh.rotation.x = -Math.PI / 2;
+		mesh.position.y = -100;
+		this.scene.add(mesh);
+	}
 
 	window.addEventListener('resize', this.resize.bind(this), false);
 	setTimeout(this.resize.bind(this), 1);
-
-	this.initObject();
-};
-
-App.prototype.initObject = function(){
-	this.fish = [];
-
-	for (var i = 0; i < fishNum; i++) {
-		this.fish.push(new Fish());
-	}
-
-	this.fish.forEach(function(f) {
-		var x = (Math.random()*(space.x[1] - space.x[0]) + space.x[0]);
-		var y = (Math.random()*(space.y[1] - space.y[0]) + space.y[0]);
-		var z = (Math.random()*(space.z[1] - space.z[0]) + space.z[0]);
-		f.setPosition(x, y, z);
-		f.setSize(.4);
-		f.setSeed(Math.random() * Math.PI * 2);
-		f.setSpeed({x: -.5, y: 0, z: 0});
-		this.scene.add(f.get3DObject());
-	}.bind(this));
-
-	this.fishes = new Fishes();
-	this.scene.add(this.fishes.getObject());
 };
 
 App.prototype.resize = function() {
@@ -183,13 +118,10 @@ App.prototype.update = function(dt) {
 
 App.prototype.render = function(dt) {
 	this.renderer.render(this.scene, this.camera);
-//	this.effect.render(this.scene, this.camera);
 };
 
 App.prototype.animate = function(t) {
 	requestAnimationFrame(this.animate.bind(this));
-
-	this.fishes.animation();
 
 	this.update(this.clock.getDelta());
 	this.render(this.clock.getDelta());
