e482adeba2a724a810540e9911789315b7e32308
refluster <refluster@gmail.com>
2016-07-16 00:02:31 +0900

    refactor no zoom

--- js/060/js/app.js
+++ js/060/js/app.js
@@ -29,7 +29,6 @@ App.prototype.init = function() {
 		this.controls = new THREE.OrbitControls(this.camera, this.element);
 		this.controls.rotateUp(Math.PI / 4);
 		this.controls.target.set(100, 100, 0);
-//		this.controls.noZoom = true;
 		this.controls.noPan = true;
 	}
 
