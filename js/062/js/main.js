6d20911a4cfcbcfb7accbb2da720f4168eef1112
refluster <refluster@gmail.com>
2017-06-24 23:41:46 +0900

    js/062 immovable object

--- js/062/js/main.js
+++ js/062/js/main.js
@@ -1,10 +1,10 @@
 var game = new Phaser.Game(400, 300, Phaser.AUTO, 'game',
-						   { preload: preload, create: create, render: render });
+						   { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 }
 
-var graphics;
+var obj, plate;
 
 function create() {
 	var spawnObj = function() {
@@ -18,19 +18,45 @@ function create() {
 		graphics.lineTo(0, 100);
 		graphics.lineTo(0, 0);
 		graphics.endFill();
-		return graphics
-	}
+		return graphics;
+	};
+
+	var spawnPlate = function() {
+		graphics = game.add.graphics(200, 150);
+		graphics.beginFill(0xa03090);
+		graphics.lineStyle(4, 0xfd02eb, 1);
+		graphics.moveTo(0, 0);
+		graphics.lineTo(125, 0);
+		graphics.lineTo(125, 10);
+		graphics.lineTo(0, 10);
+		graphics.lineTo(0, 0);
+		graphics.endFill();
+		return graphics;
+	};
 
 	obj = spawnObj();
-	game.physics.arcade.enable(obj);
+	plate = spawnPlate();
+	game.physics.arcade.enable([obj, plate]);
+	//game.physics.arcade.enable([obj]);
 
+	//game.physics.arcade.enable(obj);
 	obj.body.velocity.x = 100;
 	obj.body.velocity.y = 70;
-	//obj.body.gravity.y = 100;
+	obj.body.position.y = 270;
 	obj.body.bounce.set(1);
 	obj.body.collideWorldBounds = true;
 
+	//game.physics.arcade.enable(plate);
+	plate.body.bounce.set(1);
+	plate.body.collideWorldBounds = true;
+	plate.body.immovable = true;
+
 	console.log(obj);
+	console.log(plate);
+}
+
+function update() {
+    game.physics.arcade.collide(obj, plate);
 }
 
 function render() {
