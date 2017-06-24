9b78853c9a00845a3be985a041371689ec9344e6
refluster <refluster@gmail.com>
2017-06-24 23:19:57 +0900

    js/062 refactor

--- js/062/js/main.js
+++ js/062/js/main.js
@@ -7,27 +7,30 @@ function preload() {
 var graphics;
 
 function create() {
-	graphics = game.add.graphics(0, 0);
+	var spawnObj = function() {
+		graphics = game.add.graphics(0, 0);
+		graphics.beginFill(0xa9a904);
+		graphics.lineStyle(4, 0xfd02eb, 1);
+		graphics.moveTo(0, 0);
+		graphics.lineTo(125, 0);
+		graphics.lineTo(125, 100);
+		graphics.lineTo(62, 50);
+		graphics.lineTo(0, 100);
+		graphics.lineTo(0, 0);
+		graphics.endFill();
+		return graphics
+	}
 
-    graphics.beginFill(0xa9a904);
-    graphics.lineStyle(4, 0xfd02eb, 1);
-    
-    graphics.moveTo(0, 0);
-    graphics.lineTo(125, 0);
-    graphics.lineTo(125, 100);
-    graphics.lineTo(62, 50);
-    graphics.lineTo(0, 100);
-    graphics.lineTo(0, 0);
-    graphics.endFill();
-   
-    game.physics.arcade.enable(graphics);
+	obj = spawnObj();
+	game.physics.arcade.enable(obj);
 
-    graphics.body.velocity.x = 100;
-    graphics.body.gravity.y = 100;
-    graphics.body.bounce.set(1);
-    graphics.body.collideWorldBounds = true;
+	obj.body.velocity.x = 100;
+	obj.body.velocity.y = 70;
+	//obj.body.gravity.y = 100;
+	obj.body.bounce.set(1);
+	obj.body.collideWorldBounds = true;
 
-	console.log(graphics);
+	console.log(obj);
 }
 
 function render() {
