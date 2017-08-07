9dbdedc69cf7fc3f7b8d0d72621a25e961ba2e69
refluster <refluster@gmail.com>
2017-08-07 23:53:03 +0900

    move ball by keyboard trigger

---
+++ unity/002/lab.unity.002/Assets/NewBehaviourScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	var r = Input.GetKey("right");
+	Debug.Log(r);
+	if (r == true) {
+		transform.Translate(0.1, 0.0, 0.0);
+	}
+
+}
