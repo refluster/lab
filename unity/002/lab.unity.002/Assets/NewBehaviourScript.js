bd9541b34388abcde83e950ff7ef3327208283b8
refluster <refluster@gmail.com>
2017-08-09 00:06:46 +0900

    plain rotate by button click

--- unity/002/lab.unity.002/Assets/NewBehaviourScript.js
+++ unity/002/lab.unity.002/Assets/NewBehaviourScript.js
@@ -8,7 +8,8 @@ function Update () {
 	var r = Input.GetKey("right");
 	Debug.Log(r);
 	if (r == true) {
-		transform.Translate(0.1, 0.0, 0.0);
+		//transform.Translate(0.1, 0.0, 0.0);
+		transform.Rotate(Vector3.up);
 	}
 
 }
