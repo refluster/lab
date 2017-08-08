d1d97b6b13a9f4555296bc33986a0148795b6320
refluster <refluster@gmail.com>
2017-08-09 07:39:19 +0900

    plain rotate to oposite direction by button click

--- unity/002/lab.unity.002/Assets/NewBehaviourScript.js
+++ unity/002/lab.unity.002/Assets/NewBehaviourScript.js
@@ -5,11 +5,12 @@ function Start () {
 }
 
 function Update () {
-	var r = Input.GetKey("right");
-	Debug.Log(r);
-	if (r == true) {
+	if (Input.GetKey("right") == true) {
 		//transform.Translate(0.1, 0.0, 0.0);
-		transform.Rotate(Vector3.up);
+		transform.Rotate(1, 0.0, 0.0, Space.World);
+	} else if (Input.GetKey("left") == true) {
+		//transform.Translate(0.1, 0.0, 0.0);
+		transform.Rotate(-1, 0.0, 0.0, Space.World);
 	}
 
 }
