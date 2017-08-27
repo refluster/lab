4f068a2847b60f6bc39500c3042cefd36d13a48c
refluster <refluster@gmail.com>
2017-08-27 10:06:46 +0900

    add step function only the first code block

--- js/water-sim2/js/sim.js
+++ js/water-sim2/js/sim.js
@@ -29,31 +29,38 @@ var Dew = function(img) {
 	this.img = img;
 
 	this.R = 8;
-    this.P = 0.10000000000000001;
-    this.S = 0.02;
-    this.D = 0.050000000000000003;
-    this.Z = 0.90000000000000002;
-    this.A = 80;
-    this.B = 80;
-    this.C = 0.050000000000000003;
-    this.N = 100;
-    this.M = 0;
-    this.T = 50;
-    this.H = 1;
+	this.P = 0.10000000000000001;
+	this.S = 0.02;
+	this.D = 0.050000000000000003;
+	this.Z = 0.90000000000000002;
+	this.A = 80;
+	this.B = 80;
+	this.C = 0.050000000000000003;
+	this.N = 100;
+	this.M = 0;
+	this.T = 50;
+	this.H = 1;
 
 	particles = [];
 	neighbors = [];
 
-    for (var i = 0; i < this.N; i++) {
-        r = (Math.sqrt((i + 1) / Math.PI) * this.R * 3) / 4;
-        t = 2 * Math.sqrt(Math.PI * (i + 1));
-        x = (img.width / 2) + r * Math.cos(t);
-        y = (img.height / 2) + r * Math.sin(t);
-        particles[i] = new Particle(x, y);
-    }
-	
-    for (var i = 0; i < neighbors.length; i++) {
-        neighbors[i] = new Neighbor();
+	for (var i = 0; i < this.N; i++) {
+		r = (Math.sqrt((i + 1) / Math.PI) * this.R * 3) / 4;
+		t = 2 * Math.sqrt(Math.PI * (i + 1));
+		x = (img.width / 2) + r * Math.cos(t);
+		y = (img.height / 2) + r * Math.sin(t);
+		particles[i] = new Particle(x, y);
+	}
+
+	for (var i = 0; i < neighbors.length; i++) {
+		neighbors[i] = new Neighbor();
 	}
 };
-
+Dew.prototype.step = function() {
+	var xbit = parseInt(Math.round(Math.log(this.img.width / this.H) / Math.log(2)));
+	var ybit = parseInt(Math.round(Math.log(this.img.height / this.H) / Math.log(2)));
+	var xsize = 1 << this.xbit;
+	var ysize = 1 << this.ybit;
+	var isize = xsize * ysize;
+	var background = [];
+};
