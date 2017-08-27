c1372280af395bdb779df57564baba4407636028
refluster <refluster@gmail.com>
2017-08-27 22:18:42 +0900

    calculate density by alpha blending

--- js/062/js/main.js
+++ js/062/js/main.js
@@ -7,21 +7,27 @@ var Apl = function() {
 	this.img = this.ctx.getImageData(0, 0, this.width, this.height)
 	this.dew = new Dew(this.ctx, this.img);
 
-	var alphaCanvas = document.createElement("canvas");
-	var r = 25;
+	this.alphaGfx = document.createElement("canvas");
+	alphaCanvas = this.alphaGfx;
+	var r = 16;
+	this.dropletSize = r;
 	document.getElementById('contents').appendChild(alphaCanvas);
 	alphaCanvas.height = r*2;
 	alphaCanvas.width = r*2;
 	var alphaCtx = alphaCanvas.getContext('2d');
 	var grad = alphaCtx.createRadialGradient(r, r, 0, r, r, r);
-	grad.addColorStop(0,   'rgba(100,255,200,1');
-	grad.addColorStop(0.7, 'rgba(100,255,200,0.2)');
-	grad.addColorStop(0.9, 'rgba(100,255,200,0.05)');
-	grad.addColorStop(1,   'rgba(100,255,200,0)');
+	grad.addColorStop(0,   'rgba(128,128,224,.5');
+	grad.addColorStop(0.7, 'rgba(128,128,224,0.15)');
+	grad.addColorStop(0.9, 'rgba(128,128,224,0.025)');
+	grad.addColorStop(1,   'rgba(128,128,224,0)');
 	alphaCtx.fillStyle = grad;
 	alphaCtx.beginPath();
 	alphaCtx.arc(r, r, r, 0, Math.PI*2, true);
 	alphaCtx.fill();
+
+    alphaCtx.globalCompositeOperation="source-in";
+	alphaCtx.fillStyle = "rgb(96, 96, 224)";
+	alphaCtx.fillRect(0, 0, r*2, r*2);
 	this.alphaImage = alphaCtx.getImageData(0, 0, r*2, r*2);
 };
 Apl.prototype.blank = function() {
@@ -32,16 +38,19 @@ Apl.prototype.draw = function() {
 	this.dew.step();
 
 	{// zantei
-		this.ctx.fillStyle = "rgb(128, 128, 224)";
+		//this.ctx.fillStyle = "rgb(128, 128, 224)";
+		this.ctx.globalAlpha=1;
+		this.ctx.globalCompositeOperation = 'source-over';
 		var p = this.dew.particles;
 		for (var i = 0; i < p.length; i++) {
-			this.ctx.fillRect(p[i].x, p[i].y, 4, 4);
+			//this.ctx.fillRect(p[i].x, p[i].y, 4, 4);
+			this.ctx.drawImage(this.alphaGfx, p[i].x - this.dropletSize/2, p[i].y - this.dropletSize/2);
 		}
 	}
 
-	this.ctx.putImageData(this.alphaImage, 100, 100);
+	//this.ctx.putImageData(this.alphaImage, 100, 100);
 
-	//requestAnimationFrame(this.draw.bind(this));
+	requestAnimationFrame(this.draw.bind(this));
 };
 
 $(function() {
