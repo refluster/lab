4a0b1dd7427ac0a0b5d28daf7f4356ea606183d8
refluster <refluster@gmail.com>
2017-08-27 15:16:48 +0900

    draw sample

--- js/water-webgl/js/main.js
+++ js/water-webgl/js/main.js
@@ -3,9 +3,48 @@ var Apl = function() {
 	if ( ! canvas || ! canvas.getContext ) { return false; }
 	this.width = canvas.width;
 	this.height = canvas.height;
-	this.webgl = canvas.getContext("webgl");
+	this.gl = canvas.getContext("webgl");
 
+	var vertexShader = createShader(this.gl, 'vert-shader', this.gl.VERTEX_SHADER);
+	var fragShader = createShader(this.gl, 'frag-shader', this.gl.FRAGMENT_SHADER);
+
+	var program = this.gl.createProgram();
+	this.gl.attachShader(program, vertexShader);
+	this.gl.attachShader(program, fragShader);
+
+	this.gl.linkProgram(program);
+	this.gl.useProgram(program);
+
+	// create rectangle
+	var buffer = this.gl.createBuffer();
+	this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+	this.gl.bufferData(
+		this.gl.ARRAY_BUFFER,
+		new Float32Array([
+			-1.0, -1.0,
+			1.0, -1.0,
+			-1.0,  1.0,
+			-1.0,  1.0,
+			1.0, -1.0,
+			1.0,  1.0]),
+		this.gl.STATIC_DRAW);
+
+	// vertex data
+	var positionLocation = this.gl.getAttribLocation(program, "a_position");
+	this.gl.enableVertexAttribArray(positionLocation);
+	this.gl.vertexAttribPointer(positionLocation, 2, this.gl.FLOAT, false, 0, 0);
+
+	this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
 };
+
+function createShader(gl, source,type){
+	var shader = gl.createShader(type);
+	source = document.getElementById(source).text;
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+
+	return shader;
+}
 
 $(function() {
 	apl = new Apl();
