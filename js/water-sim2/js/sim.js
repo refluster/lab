7bb16506a1f4268456de6ccc2c3c6b9c3d5f315c
refluster <refluster@gmail.com>
2017-08-27 11:28:08 +0900

    refactor by removing synchronized block at java

--- js/water-sim2/js/sim.js
+++ js/water-sim2/js/sim.js
@@ -117,138 +117,132 @@ Dew.prototype.step = function() {
 		do {
 			if (t >= this.T)
 				break;
-			{
-				for (var i = 0; i < this.N; i++) {
-					p = particles[i];
-					var dx = p.x >= xmin ? p.x >= xmax ? p.x - xmax : 0.0 : p.x - xmin;
-					var dy = p.y >= ymin ? p.y >= ymax ? p.y - ymax : 0.0 : p.y - ymin;
-					var d2 = dx * dx + dy * dy;
-					if (d2 > 1.0) {
-						var d = Math.sqrt(d2);
-						p.vx -= ((d - 1) * dx) / d;
-						p.vy -= ((d - 1) * dy) / d;
+			for (var i = 0; i < this.N; i++) {
+				p = particles[i];
+				var dx = p.x >= xmin ? p.x >= xmax ? p.x - xmax : 0.0 : p.x - xmin;
+				var dy = p.y >= ymin ? p.y >= ymax ? p.y - ymax : 0.0 : p.y - ymin;
+				var d2 = dx * dx + dy * dy;
+				if (d2 > 1.0) {
+					var d = Math.sqrt(d2);
+					p.vx -= ((d - 1) * dx) / d;
+					p.vy -= ((d - 1) * dy) / d;
+				}
+				p.x += p.vx;
+				p.y += p.vy;
+				p.tag = (p.y / this.R) << 16 | (p.x / this.R);
+				p.w = 0;
+				p.nx = 0;
+				p.ny = 0;
+			}
+
+			this.M = 0;
+			this.sort(particles, 0, this.N - 1);
+			var i = 0;
+			var j = 0;
+			label0:
+			for(; i < this.N; i++) {
+				p = particles[i];
+				var k = i + 1;
+				do {
+					if (k >= this.N) {
+						break;
 					}
-					p.x += p.vx;
-					p.y += p.vy;
-					p.tag = (p.y / this.R) << 16 | (p.x / this.R);
-					p.w = 0;
-					p.nx = 0;
-					p.ny = 0;
-				}
-
-				this.M = 0;
-				this.sort(particles, 0, this.N - 1);
-				var i = 0;
-				var j = 0;
-				label0:
-				for(; i < this.N; i++) {
-					p = particles[i];
-					var k = i + 1;
-					do {
-						if (k >= this.N) {
-							break;
-						}
-						q = particles[k];
-						if (p.tag + 1 < q.tag) {
-							break;
-						}
-						this.match(neighbors, p, q);
-						k++;
-					} while(true);
-					do {
-						if (j >= this.N) {
-							break;
-						}
-						q = particles[j];
-						if ((p.tag + 0x10000) - 1 <= q.tag) {
-							break;
-						}
-						j++;
-					} while(true);
-					q = j;
-					do {
-						if (q >= this.N) {
-							continue label0;
-						}
-						q = particles[q];
-						if (p.tag + 0x10000 + 1 < q.tag) {
-							continue label0;
-						}
-						this.match(neighbors, p, q);
-						q++;
-					} while(true);
-				}
-
-				for (var i = 0; i < this.M; i++) {
-					var n = neighbors[i];
-					var a = n.a;
-					var b = n.b;
-					var w = n.w;
-					var nx = n.nx;
-					var ny = n.ny;
-					a.w += w;
-					b.w += w;
-					a.nx -= w * nx;
-					a.ny -= w * ny;
-					b.nx += w * nx;
-					b.ny += w * ny;
-				}
-
-				for (var i = 0; i < this.N; i++) {
-					var p = particles[i];
-					p.h = this.P * (p.w - 1);
-					p.sx = this.S * this.R * p.nx;
-					p.sy = this.S * this.R * p.ny;
-					p.fx = 0;
-					p.fy = 0;
-				}
-
-				for (var i = 0; i < this.M; i++) {
-					var n = neighbors[i];
-					var a = n.a;
-					var b = n.b;
-					var r = n.r;
-					var w = n.w;
-					var nx = n.nx;
-					var ny = n.ny;
-					var fn = (a.h + b.h) * w;
-					var vx = b.vx - a.vx;
-					var vy = b.vy - a.vy;
-					var vn = vx * nx + vy * ny;
-					if (vn < 0) {
-						fn -= this.D * vn;
+					q = particles[k];
+					if (p.tag + 1 < q.tag) {
+						break;
 					}
-					var sx = b.sx - a.sx;
-					var sy = b.sy - a.sy;
-					var sn = sx * nx + sy * ny;
-					var fx = fn * nx + (sn * nx + (w * sx) / 2) / r;
-					var fy = fn * ny + (sn * ny + (w * sy) / 2) / r;
-					a.fx -= fx;
-					a.fy -= fy;
-					b.fx += fx;
-					b.fy += fy;
-				}
+					this.match(neighbors, p, q);
+					k++;
+				} while(true);
+				do {
+					if (j >= this.N) {
+						break;
+					}
+					q = particles[j];
+					if ((p.tag + 0x10000) - 1 <= q.tag) {
+						break;
+					}
+					j++;
+				} while(true);
+				q = j;
+				do {
+					if (q >= this.N) {
+						continue label0;
+					}
+					q = particles[q];
+					if (p.tag + 0x10000 + 1 < q.tag) {
+						continue label0;
+					}
+					this.match(neighbors, p, q);
+					q++;
+				} while(true);
+			}
 
-				for (var i = 0; i < this.N; i++) {
-					p = particles[i];
-					p.vx += p.fx;
-					p.vy += p.fy;
+			for (var i = 0; i < this.M; i++) {
+				var n = neighbors[i];
+				var a = n.a;
+				var b = n.b;
+				var w = n.w;
+				var nx = n.nx;
+				var ny = n.ny;
+				a.w += w;
+				b.w += w;
+				a.nx -= w * nx;
+				a.ny -= w * ny;
+				b.nx += w * nx;
+				b.ny += w * ny;
+			}
+
+			for (var i = 0; i < this.N; i++) {
+				var p = particles[i];
+				p.h = this.P * (p.w - 1);
+				p.sx = this.S * this.R * p.nx;
+				p.sy = this.S * this.R * p.ny;
+				p.fx = 0;
+				p.fy = 0;
+			}
+
+			for (var i = 0; i < this.M; i++) {
+				var n = neighbors[i];
+				var a = n.a;
+				var b = n.b;
+				var r = n.r;
+				var w = n.w;
+				var nx = n.nx;
+				var ny = n.ny;
+				var fn = (a.h + b.h) * w;
+				var vx = b.vx - a.vx;
+				var vy = b.vy - a.vy;
+				var vn = vx * nx + vy * ny;
+				if (vn < 0) {
+					fn -= this.D * vn;
 				}
+				var sx = b.sx - a.sx;
+				var sy = b.sy - a.sy;
+				var sn = sx * nx + sy * ny;
+				var fx = fn * nx + (sn * nx + (w * sx) / 2) / r;
+				var fy = fn * ny + (sn * ny + (w * sy) / 2) / r;
+				a.fx -= fx;
+				a.fy -= fy;
+				b.fx += fx;
+				b.fy += fy;
+			}
+
+			for (var i = 0; i < this.N; i++) {
+				p = particles[i];
+				p.vx += p.fx;
+				p.vy += p.fy;
 			}
 			t++;
-		    //break; // zantei
 		} while(true);
 
 		density.source.fill(0);
-		{
-			for(var i = 0; i < this.N; i++) {
-				p = particles[i];
-				var x = Math.max(1, Math.min((xscale * p.x), xsize - 1));
-				var y = Math.max(1, Math.min((yscale * p.y), ysize - 1));
-				var j = x | y << xbit;
-				density.source[j] += 8;
-			}
-
+		for(var i = 0; i < this.N; i++) {
+			p = particles[i];
+			var x = Math.max(1, Math.min((xscale * p.x), xsize - 1));
+			var y = Math.max(1, Math.min((yscale * p.y), ysize - 1));
+			var j = x | y << xbit;
+			density.source[j] += 8;
 		}
 		density.solve(1, 2);
 
@@ -334,45 +328,43 @@ Dew.prototype.step = function() {
 		}
 		photons.solve(1, 2);
 
-		{
-			/*
-			if (background != null) {
-				for(int i = 0; i < isize; i++)
-				{
-					int j = refraction[i];
-					double specular = reflection[i];
-					specular *= specular;
-					specular *= specular;
-					specular *= specular;
-					specular *= specular;
-					int white = (int)(65536D * specular);
-					int luminance = (int)(A + B * C * photons.value[j]);
-					int color = background[j];
-					int red = white + luminance * (color >> 16 & 0xff);
-					int green = white + luminance * (color >> 8 & 0xff);
-					int blue = white + luminance * (color & 0xff);
-					pixels[i] = 0xff000000 | 0xff0000 & (red | -(red >> 16) >> 31) << 8 | 0xff00 & (green | -(green >> 16) >> 31) | 0xff & (blue | -(blue >> 16) >> 31) >> 8;
-				}
-
-			} else
-			*/
+		/*
+		if (background != null) {
+			for(int i = 0; i < isize; i++)
 			{
-				for (var i = 0; i < isize; i++) {
-					var j = refraction[i];
-					var specular = reflection[i];
-					specular *= specular;
-					specular *= specular;
-					specular *= specular;
-					specular *= specular;
-					var white = parseInt(256 * specular);
-					white += parseInt(this.A + this.B * this.C * photons.value[j]);
-					white = (white | -(white >> 8) >> 31) & 0xff;
-					//pixels[i] = 0xff000000 | white << 16 | white << 8 | white;
-					this.img.data[i*4 + 0] = white;
-					this.img.data[i*4 + 1] = white;
-					this.img.data[i*4 + 2] = white;
-					this.img.data[i*4 + 3] = 255;
-				}
+				int j = refraction[i];
+				double specular = reflection[i];
+				specular *= specular;
+				specular *= specular;
+				specular *= specular;
+				specular *= specular;
+				int white = (int)(65536D * specular);
+				int luminance = (int)(A + B * C * photons.value[j]);
+				int color = background[j];
+				int red = white + luminance * (color >> 16 & 0xff);
+				int green = white + luminance * (color >> 8 & 0xff);
+				int blue = white + luminance * (color & 0xff);
+				pixels[i] = 0xff000000 | 0xff0000 & (red | -(red >> 16) >> 31) << 8 | 0xff00 & (green | -(green >> 16) >> 31) | 0xff & (blue | -(blue >> 16) >> 31) >> 8;
+			}
+
+		} else
+		*/
+		{
+			for (var i = 0; i < isize; i++) {
+				var j = refraction[i];
+				var specular = reflection[i];
+				specular *= specular;
+				specular *= specular;
+				specular *= specular;
+				specular *= specular;
+				var white = parseInt(256 * specular);
+				white += parseInt(this.A + this.B * this.C * photons.value[j]);
+				white = (white | -(white >> 8) >> 31) & 0xff;
+				//pixels[i] = 0xff000000 | white << 16 | white << 8 | white;
+				this.img.data[i*4 + 0] = white;
+				this.img.data[i*4 + 1] = white;
+				this.img.data[i*4 + 2] = white;
+				this.img.data[i*4 + 3] = 255;
 			}
 		}
 		break;
