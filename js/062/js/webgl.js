99178a3e63ad20837c2abe3a2ae4cee0b6f7d519
refluster <refluster@gmail.com>
2017-09-09 23:38:27 +0900

    refactor

--- js/062/js/webgl.js
+++ js/062/js/webgl.js
@@ -36,74 +36,17 @@ function loadShader(gl, type, source) {
 	return shader;
 }
 
-function loadTexture(gl, image) {
-	// Because images have to be download over the internet
-	// they might take a moment until they are ready.
-	// Until then put a single pixel in the texture so we can
-	// use it immediately. When the image has finished downloading
-	// we'll update the texture with the contents of the image.
-	const level = 0;
-	const internalFormat = gl.RGBA;
-	const width = 1;
-	const height = 1;
-	const border = 0;
-	const srcFormat = gl.RGBA;
-	const srcType = gl.UNSIGNED_BYTE;
-	const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-	const texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                  width, height, border, srcFormat, srcType,
-                  pixel);
-
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-				  srcFormat, srcType, image);
-
-	// WebGL1 has different requirements for power of 2 images
-	// vs non power of 2 images so check if the image is a
-	// power of 2 in both dimensions.
-	if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-		// Yes, it's a power of 2. Generate mips.
-		gl.generateMipmap(gl.TEXTURE_2D);
-	} else {
-		// No, it's not a power of 2. Turn of mips and set
-		// wrapping to clamp to edge
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	}
-
-	return texture;
-}
-
 function isPowerOf2(value) {
   return (value & (value - 1)) == 0;
 }
 
-function uniformTexture(gl, uniformLocation, texture) {
-	// Tell WebGL we want to affect texture unit 0
-	gl.activeTexture(gl.TEXTURE0);
-
-	// Bind the texture to texture unit 0
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-
-	// Tell the shader we bound the texture to texture unit 0
-	gl.uniform1i(uniformLocation, 0);
-
-	// Bind the texture to texture unit 0
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-}
-
-//function drawScene(gl, programInfo, buffers, texture, deltaTime) {
-function drawScene(gl, programInfo, buffers, texture, deltaTime) {
+function drawScene(gl, programInfo, buffers) {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
 	gl.clearDepth(1.0);                 // Clear everything
 	gl.enable(gl.DEPTH_TEST);           // Enable depth testing
 	gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
 
 	// Clear the canvas before we start drawing on it.
-
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	// Tell WebGL which indices to use to index the vertices
@@ -112,15 +55,6 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 	// Tell WebGL to use our program when drawing
 	gl.useProgram(programInfo.program);
 
-	// Tell WebGL we want to affect texture unit 0
-	//gl.activeTexture(gl.TEXTURE0);
-
-	// Bind the texture to texture unit 0
-	//gl.bindTexture(gl.TEXTURE_2D, texture);
-
-	// Tell the shader we bound the texture to texture unit 0
-	//gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
-
 	{
 		const vertexCount = 6;
 		const first = 0;
@@ -128,7 +62,6 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 	}
 }
 
-////////////////////////////////////////////////////////////
 function createTexture(gl, source, i) {
 	var texture = gl.createTexture();
 	activeTexture(gl, i);
