75583244e93b48dc44317b7621d171fdf53a21ba
refluster <refluster@gmail.com>
2017-10-07 02:47:42 +0900

    refactor (not work)

--- js/sensor-graph/js/fridge.js
+++ js/sensor-graph/js/fridge.js
@@ -5,26 +5,9 @@ Apl = function() {
 			accelerationIncludingGravity: {x: '-', y: '-', z: '-'},
 			acceleration: {x: '-', y: '-', z: '-'},
 			rotationRate: {alpha: '-', beta: '-', gamma: '-'}},
-        deviceorientation: {absolute: '-', alpha: '-', beta: '-', gamma: '-'},
-        devicelight: {value: '-'},
-        deviceproximity: {max: '-', min: '-', value: '-'},
 	};
 
 	$('#save').on('click', this.startSensing.bind(this));
-
-	var d = $('#sense tbody');
-	d.append('<tr><td>acceleration including gravity</td>' +
-			 '<td>' + (window.DeviceMotionEvent? 'ok': '-') + '</td></tr>');
-	d.append('<tr><td>acceleration</td>' +
-			 '<td>' + (window.DeviceMotionEvent? 'ok': '-') + '</td></tr>');
-	d.append('<tr><td>rotation rate</td>' +
-			 '<td>' + (window.DeviceMotionEvent? 'ok': '-') + '</td></tr>');
-	d.append('<tr><td>gyroscope</td>' +
-			 '<td>' + (window.DeviceOrientationEvent? 'ok': '-') + '</td></tr>');
-	d.append('<tr><td>ambient light</td>' +
-			 '<td>' + (window.DeviceLightEvent? 'ok': '-') + '</td></tr>');
-	d.append('<tr><td>proximity</td>' +
-			 '<td>' + (window.DeviceProximityEvent? 'ok': '-') + '</td></tr>');
 
 	$('#status').text('ready');
 
@@ -58,26 +41,11 @@ Apl = function() {
 };
 
 Apl.prototype.startSensing = function(e) {
-	this.sampleInterval = parseInt($('#sampling-interval').val() || '200'); // msec
+	this.sampleInterval = 10;
 
 	if (window.DeviceMotionEvent) {
 		$(window).on('devicemotion', function(e) {
 			this.ePointers.devicemotion = e.originalEvent;
-		}.bind(this));
-	}
-	if (window.DeviceOrientationEvent) {
-		$(window).on('deviceorientation', function(e) {
-			this.ePointers.deviceorientation = e.originalEvent;
-		}.bind(this));
-	}
-	if (window.DeviceLightEvent) {
-		$(window).on('devicelight', function(e) {
-			this.ePointers.devicelight = e.originalEvent;
-		}.bind(this));
-	}
-	if (window.DeviceProximityEvent) {
-		$(window).on('deviceproximity', function(e) {
-			this.ePointers.deviceproximity = e.originalEvent;
 		}.bind(this));
 	}
 	this.socket.emit('logreset');
@@ -91,15 +59,6 @@ Apl.prototype.startSensing = function(e) {
 Apl.prototype.stopSensing = function(e) {
 	if (window.DeviceMotionEvent) {
 		$(window).off('devicemotion');
-	}
-	if (window.DeviceOrientationEvent) {
-		$(window).off('deviceorientation');
-	}
-	if (window.DeviceLightEvent) {
-		$(window).off('devicelight');
-	}
-	if (window.DeviceProximityEvent) {
-		$(window).off('deviceproximity');
 	}
 	clearInterval(this.timer);
 	$('#save').text('start');
@@ -128,14 +87,6 @@ Apl.prototype.logDataPush = function() {
 		this.ePointers.devicemotion.rotationRate.alpha,
 		this.ePointers.devicemotion.rotationRate.beta,
 		this.ePointers.devicemotion.rotationRate.gamma,
-		this.ePointers.deviceorientation.absolute,
-		this.ePointers.deviceorientation.alpha,
-		this.ePointers.deviceorientation.beta,
-		this.ePointers.deviceorientation.gamma,
-		this.ePointers.devicelight.value,
-		this.ePointers.deviceproximity.max,
-		this.ePointers.deviceproximity.min,
-		this.ePointers.deviceproximity.value,
 	];
 	this.socket.emit('collect', {
 		data: newRecord
@@ -148,50 +99,6 @@ Apl.prototype.logDateFormat = function(data) {
 	data.forEach(function(d) {
 		d.date = parseTime(d[0]) - beginDate;
 	});
-};
-
-Apl.prototype.drawGraph = function(sensor_idx, start, end) {
-	var svg = d3.select("svg");
-	svg.selectAll('*')
-		.remove();
-
-	var svg = d3.select("svg"),
-		margin = {top: 20, right: 20, bottom: 30, left: 50},
-		width = 800 - margin.left - margin.right,
-		height = 400 - margin.top - margin.bottom,
-		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	var x = d3.scaleTime()
-		.rangeRound([0, width]);
-
-	var y = d3.scaleLinear()
-		.rangeRound([height, 0]);
-
-	var line = d3.line()
-		.x(function(d) { return x(d.date); })
-		.y(function(d) { return y(d[sensor_idx]); });
-
-	function drawLine(data) {
-		x.domain([start, end]);
-		y.domain([-10, 10]); // due to gravity data (-9.8 - +9.8)
-
-		g.append("g")
-			.attr("transform", "translate(0," + height + ")")
-			.call(d3.axisBottom(x))
-			.select(".domain")
-			.remove();
-
-		g.append("path")
-			.datum(data)
-			.attr("fill", "none")
-			.attr("stroke", "steelblue")
-			.attr("stroke-linejoin", "round")
-			.attr("stroke-linecap", "round")
-			.attr("stroke-width", 1.5)
-			.attr("d", line);
-	}
-	drawLine(this.log);
-	drawLine(this.pro_history);
 };
 
 var apl; // for debug
