ce6a9cff39f9df7f7885cacbe0e62bf87315cb31
refluster <refluster@gmail.com>
2017-10-06 09:48:13 +0900

    draw graph from tsv

--- js/sensor-graph/js/main.js
+++ js/sensor-graph/js/main.js
@@ -80,7 +80,64 @@ Apl.prototype.logDataPush = function() {
 	});
 };
 
+Apl.prototype.drawGraph = function() {
+	var svg = d3.select("svg"),
+		margin = {top: 20, right: 20, bottom: 30, left: 50},
+		width = 300 - margin.left - margin.right,
+		height = 300 - margin.top - margin.bottom,
+		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var parseTime = d3.timeParse("%d-%b-%y");
+
+	var x = d3.scaleTime()
+		.rangeRound([0, width]);
+
+	var y = d3.scaleLinear()
+		.rangeRound([height, 0]);
+
+	var line = d3.line()
+		.x(function(d) { return x(d.date); })
+		.y(function(d) { return y(d.close); });
+
+	d3.csv("data.csv", function(d) {
+		d.date = parseTime(d.date);
+		d.close = +d.close;
+		return d;
+	}, function(error, data) {
+		if (error) throw error;
+
+		x.domain(d3.extent(data, function(d) { return d.date; }));
+		y.domain(d3.extent(data, function(d) { return d.close; }));
+
+		g.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x))
+			.select(".domain")
+			.remove();
+
+		g.append("g")
+			.call(d3.axisLeft(y))
+			.append("text")
+			.attr("fill", "#000")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", "0.71em")
+			.attr("text-anchor", "end")
+			.text("Price ($)");
+
+		g.append("path")
+			.datum(data)
+			.attr("fill", "none")
+			.attr("stroke", "steelblue")
+			.attr("stroke-linejoin", "round")
+			.attr("stroke-linecap", "round")
+			.attr("stroke-width", 1.5)
+			.attr("d", line);
+	});
+};
+
 var apl; // for debug
 $(function() {
 	apl = new Apl();
+	apl.drawGraph();
 });
