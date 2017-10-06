36ace21498e9ee0ec4fd029cac3dba4425b82654
refluster <refluster@gmail.com>
2017-10-06 11:49:09 +0900

    refactor and data read of pro history

--- js/sensor-graph/js/main.js
+++ js/sensor-graph/js/main.js
@@ -23,12 +23,16 @@ Apl = function() {
 	this.socket = io.connect('http://lab.schememono.net:8881');
 	this.socket.on('log', function(d) {
 		this.log.push(d.data);
+		this.logDateFormat(this.log);
 		this.drawGraph(this.log);
 		this.tick ++;
 	}.bind(this));
 	this.socket.on('logreset', function() {
 		this.log = [];
 	}.bind(this));
+
+	this.pro_history = JSON.parse(pro_history);
+	this.logDateFormat(this.pro_history);
 };
 
 Apl.prototype.startSaving = function(e) {
@@ -82,6 +86,14 @@ Apl.prototype.logDataPush = function() {
 	});
 };
 
+Apl.prototype.logDateFormat = function(data) {
+	var parseTime = d3.timeParse('%Y/%m/%d %H:%M:%S.%L');	
+	var beginDate = parseTime(data[0][0]);
+	data.forEach(function(d) {
+		d.date = parseTime(d[0]) - beginDate;
+	});
+};
+
 Apl.prototype.drawGraph = function(data) {
 	var svg = d3.select("svg");
 	svg.selectAll('*')
@@ -94,13 +106,13 @@ Apl.prototype.drawGraph = function(data) {
 		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	//var parseTime = d3.timeParse("%d-%b-%y");
-	var parseTime = d3.timeParse('%Y/%m/%d %H:%M:%S.%L');
+	//var parseTime = d3.timeParse('%Y/%m/%d %H:%M:%S.%L');
 	//2017/10/05 18:02:55.251
 
-	var beginDate = parseTime(data[0][0]);
-	data.forEach(function(d) {
-		d.date = parseTime(d[0]) - beginDate;
-	});
+	//var beginDate = parseTime(data[0][0]);
+	//data.forEach(function(d) {
+	//	d.date = parseTime(d[0]) - beginDate;
+	//});
 
 	var x = d3.scaleTime()
 		.rangeRound([0, width]);
@@ -112,24 +124,26 @@ Apl.prototype.drawGraph = function(data) {
 		.x(function(d) { return x(d.date); })
 		.y(function(d) { return y(d[1]); });
 
-	x.domain(d3.extent(data, function(d) { return d.date; }));
-	//y.domain([0, d3.max(data, function(d) { return d[1]; })]);
-	y.domain([-10, 10]); // due to gravity data (-9.8 - +9.8)
+	function drawLine(data) {
+		x.domain(d3.extent(data, function(d) { return d.date; }));
+		y.domain([-10, 10]); // due to gravity data (-9.8 - +9.8)
 
-	g.append("g")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x))
-		.select(".domain")
-		.remove();
+		g.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x))
+			.select(".domain")
+			.remove();
 
-	g.append("path")
-		.datum(data)
-		.attr("fill", "none")
-		.attr("stroke", "steelblue")
-		.attr("stroke-linejoin", "round")
-		.attr("stroke-linecap", "round")
-		.attr("stroke-width", 1.5)
-		.attr("d", line);
+		g.append("path")
+			.datum(data)
+			.attr("fill", "none")
+			.attr("stroke", "steelblue")
+			.attr("stroke-linejoin", "round")
+			.attr("stroke-linecap", "round")
+			.attr("stroke-width", 1.5)
+			.attr("d", line);
+	}
+	drawLine(data);
 };
 
 var apl; // for debug
